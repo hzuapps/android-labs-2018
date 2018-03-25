ff8c41be9f77392a949bdbdd74d5e83b1656e932
JingZXian <1016054954@qq.com>
2018-03-25 18:46:45 +0800

    #1 first lab

--- soft1614080902334/soft1614080902334Activity.java
+++ soft1614080902334/soft1614080902334Activity.java
@@ -2,7 +2,7 @@ package edu.hzuapps.androidlabs.soft1614080902334;
 
 //
 public class Soft1614080902334Activity {
-	public void staic main(String[] args){
+	public void static main(String[] args){
 	
 	}
   
