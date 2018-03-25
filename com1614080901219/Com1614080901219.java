907962434cf2e850cd852a1f69a09d6f76e2d3f5
as6296463 <37761817+as6296463@users.noreply.github.com>
2018-03-26 00:34:05 +0800

    Update and rename com1614080901219.java to Com1614080901219.java

---
+++ com1614080901219/Com1614080901219.java
@@ -0,0 +1,5 @@
+package edu.hzuapps.androidlabs.Com1614080901219
+
+public class Com1614080901219 {
+	
+}
--- com1614080901219/com1614080901219.java
+++
@@ -1,5 +0,0 @@
-package edu.hzuapps.androidlabs.com1614080901219
-
-public class com1614080901219 {
-	
-}
