a0a8dd86a8cd17f6e28b04b2f4f9e842931e1e53
unknown <953336739@qq.com>
2018-03-25 18:36:55 +0800

    #1 第1次实验

--- soft1614080902425/edu.hzuapps.androidlabs.soft1614080902425/Soft1614080902425Activity.java
+++ soft1614080902425/edu.hzuapps.androidlabs.soft1614080902425/Soft1614080902425Activity.java
@@ -1,5 +1,5 @@
 package edu.hzuapps.androidlabs.soft1614080902425
-public class SOft1614080902425Activity{
+public class Soft1614080902425Activity
 	public static void main(Sting[] args){
 		System.out.println("hello world");
 	}
