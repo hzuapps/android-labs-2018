fb2ee221e1f6169b42db50b8b048d53a9084c56b
yeyongqi <1035069103@qq.com>
2018-03-25 23:50:19 +0800

    #1 com1614080901127第一次实验

--- com1614080901127/Tea123456Activity1.java
+++
@@ -1,5 +0,0 @@
-edu.hzuapps.androidlabs.soft123456.com1614080901127.java
-
-public class Tea123456Activity1{
-  
-}
---
+++ com1614080901127/com1614080901127.java
@@ -0,0 +1,6 @@
+package edu.hzuapps.androidlabs.com1614080901127;
+
+// 
+public class com1614080901127ctivity {
+  
+}
