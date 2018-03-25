5f717c8ab99846d1d6d624cb7830500364adc1b9
unknown <>
2018-03-25 17:21:45 +0800

    #1 第一次实验

--- t123456/Com1614080901237Activity.java
+++ t123456/Com1614080901237Activity.java
@@ -1,6 +1,6 @@
 package edu.hzuapps.androidlabs.com1614080901237;
 
-public class Com16140801237Activity{
+public class Com1614080901237Activity{
 	public static void main(String[] args)
 		{
 			System.out.println("Hello World!");
