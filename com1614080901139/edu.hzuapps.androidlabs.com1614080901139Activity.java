29c7b3e13df262ea48ed21a6de1e7505e856177c
DESKTOP-UEK8I3H\Admin <945451501@qq.com>
2018-03-26 10:17:35 +0800

    #1 #spemed 第一次实验

--- com1614080901139/edu.hzuapps.androidlabs.com1614080901139Activity.java
+++ com1614080901139/edu.hzuapps.androidlabs.com1614080901139Activity.java
@@ -1,5 +1,5 @@
 package edu.hzuapps.androidlabs.com1614080901139Activity
-public class com1614080901139Activity{
+public class Com1614080901139Activity{
 	public static void main(String args[]){
 		System.out.print("PHP JAVA");
 	}
