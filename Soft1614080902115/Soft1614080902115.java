e5aed325af17ac2bb675d3c33118ba1e4d651eb4
unknown <>
2018-03-26 20:59:39 +0800

    ssss

--- Soft1614080902115/Soft1614080902115.java
+++
@@ -1,5 +0,0 @@
-package edu.hzuapps.androidlabs.Soft1614080902115;
-
-public class Soft1614080902115Activity {
-  
-}
--- soft1614080902345/soft1614080902345Activity.java
+++
@@ -1,8 +0,0 @@
-package edu.hzuapps.androidlabs.soft1614080902345;
-
-
-
-//
-public class Soft1614080902345Activity {
-  
-}
