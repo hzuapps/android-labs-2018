15a30fe1bf14a9ab2620c7a16f8198cf11fab181
hzutanziyang <1194738143@qq.com>
2018-03-26 15:00:47 +0800

    #1 第一次实验

--- soft1614080902227Activity.java
+++ soft1614080902227Activity.java
@@ -1,6 +1,5 @@
-package edu.hzuapps.androidlabs.soft1614080902227£»
-
-public class Soft1614080902227 Activity {
+package edu.hzuapps.androidlabs.soft1614080902227;
+public class Soft1614080902227Activity {
 
 
 }
