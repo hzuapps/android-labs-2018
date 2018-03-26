c7ffa97be5fe05d5398cb6a35d14d87f9e741b53
Zhengmianjie <37769410+Zhengmianjie@users.noreply.github.com>
2018-03-26 17:43:19 +0800

    Update Soft1614080902314Activity.java

--- soft1614080902314/Soft1614080902314Activity.java
+++ soft1614080902314/Soft1614080902314Activity.java
@@ -1,5 +1,6 @@
-package edu.hzuapps.androidlabs.Soft1614080902314;
+package edu.hzuapps.androidlabs.soft1614080902314;
 
 public class Soft1614080902314Activity {
-  //*guoguoguo
+  //guoguoguo
+  //qiulaoshigeiguo
 }
