456b4bc69c9d94b858fe5b273761373f3904e1dc
xiamilin <1291561148@qq.com>
2018-03-26 13:36:36 +0800

    #1 #1614080901244 第1次实验

--- com1614080901244/edu.hzuapps.androidlabs.com1614080901244.Com1614080901244Activity.java
+++
@@ -1,6 +0,0 @@
-package edu.hzuapps.androidlabs.com1614080901244 ;
-public class edu.hzuapps.androidlabs.com1614080901244.Com1614080901244Activity {
-	public static void main(String args[]) {
-		System.out.println("Hello world");
-	}
-}
