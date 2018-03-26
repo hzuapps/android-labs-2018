2ee9ddf7365a81dcf7cdc35054214f71ad42ef2b
heweisheng <953336739@qq.com>
2018-03-26 14:59:32 +0800

    #1 第一次作业

--- soft1614080902425/Soft1614080902425Activity.java
+++ soft1614080902425/Soft1614080902425Activity.java
@@ -1,7 +1,6 @@
-package edu.hzuapps.android-labs-2018.soft1614080902425
+package edu.hzuapps.androidlabs.soft1614080902425;
 public class Soft1614080902425Activity
-	public static void main(String[] args){
-		System.out.println("hello world");
-	}
+	{
+	
 }
 
