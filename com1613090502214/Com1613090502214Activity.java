3b9945b73be30fad6517c2be57bc330f8afa7fcb
PickForSth <1072054112@qq.com>
2018-03-26 12:19:55 +0800

    #1 第一次实验

---
+++ com1613090502214/Com1613090502214Activity.java
@@ -0,0 +1,5 @@
+package edu.hzuapps.androidlabs.com1613090502214;
+
+public class Com1613090502214Activity {
+  
+}
--- com1613090502214/edu.hzuapps.androidlabs.com1613090502214.Com1613090502214Activity.java
+++
@@ -1,5 +0,0 @@
-package edu.hzuapps.androidlabs.com1613090502214;
-
-public class edu.hzuapps.androidlabs.com1613090502214.Com1613090502214Activity {
-  
-}
