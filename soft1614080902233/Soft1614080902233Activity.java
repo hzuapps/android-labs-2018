a837d20325053d4ea731f7256f6bb7069690fb5f
unknown <>
2018-03-31 18:16:21 +0800

    #1 第一次实验

--- soft1614080902233/Soft1614080902233Activity.java
+++ soft1614080902233/Soft1614080902233Activity.java
@@ -1,5 +1,5 @@
-package edu.hzuapps.androidlabs.soft1614080902233;
+package edu.hzuapps.androidlabs.Soft1614080902233
 
-public class Soft16140809022333Activity{
+public class Soft1614080902233Activity{
 	
 }
