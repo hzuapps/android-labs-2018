e4865eb4e0419b00ebc1a5a2a7d6fbb16e0136cf
zjy869827329 <869827329@qq.com>
2018-04-02 21:26:47 +0800

    #1 zjy869827329第一次实验

---
+++ soft1614080902115/Soft1614080902115Activity.java
@@ -0,0 +1,4 @@
+import edu.hzuapps.androidlabs.soft1614080902115;
+public class Soft1614080902115Activity{
+
+}
--- soft1614080902115/Soft1614080902434Activity.java
+++
@@ -1,6 +0,0 @@
-package edu.hzuapps.androidlabs.soft1614080902115;
-
-// 
-public class Soft1614080902115Activity {
-  
-}
