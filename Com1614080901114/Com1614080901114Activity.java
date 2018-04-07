18f7ad412b73e6e643ab19876b1b48f199936feb
Transcender <1196213885@qq.com>
2018-04-07 21:42:20 +0800

    delete file

--- Com1614080901114/Com1614080901114Activity.java
+++
@@ -1,5 +0,0 @@
-package edu.hzuapps.androidlabs.Com1614080901114;
-
-public class Com1614080901114Activity {
-  
-}
--- Com1614080901142/Com1614080901142Activity.java
+++
@@ -1,5 +0,0 @@
-package edu.hzuapps.androidlabs.Com1614080901142;
-
-public class Com1614080901142Activity {
-  
-}
