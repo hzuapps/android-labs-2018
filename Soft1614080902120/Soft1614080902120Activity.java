5424d3099b23d83504b53c56aadc8e20e77e1ddc
Transcender <1196213885@qq.com>
2018-04-07 22:01:20 +0800

    delete file

--- Soft1614080902120/Soft1614080902120Activity.java
+++
@@ -1,4 +0,0 @@
-package edu.hzuapps.androidlabs.soft1614080902120;
-
-public class Soft1614080902120Activity{
-};
--- Soft1614080902145/Soft1614080902145Activity.java
+++
@@ -1,7 +0,0 @@
-package edu.hzuapps.androidlabs.Soft1614080902145Activity;
-
-public class Soft1614080902145Activity{
-
-
-
-}
