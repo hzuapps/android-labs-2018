9917fd9b42869914c71d55221e9f18a7cbcf821c
Zmingcheng <37758518+Zmingcheng@users.noreply.github.com>
2018-04-15 10:14:54 +0800

    #2 #894 第二次实验QQ音乐

---
+++ soft1614080902121/Soft1614080902121Activity.java
@@ -0,0 +1,13 @@
+package soft1614080902121.androidlabs.hzuapps.edu.soft1614080902121activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Soft1614080902121Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_soft1614080902121);
+    }
+}
