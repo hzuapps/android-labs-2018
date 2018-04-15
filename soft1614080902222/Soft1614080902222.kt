3a8df57ff10528ea2c45367c49dd9c589a156147
BFFat <37758581+BFFat@users.noreply.github.com>
2018-04-15 12:18:32 +0800

    Add files via upload

---
+++ soft1614080902222/Soft1614080902222.kt
@@ -0,0 +1,12 @@
+package edu.huzapps.androidlabs.soft1614080902222
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Soft1614080902222 : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_soft1614080902222)
+    }
+}
