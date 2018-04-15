b35a1cff5f058e1bbcd2746948927608d5259a5d
GhostITS <37364526+GhostITS@users.noreply.github.com>
2018-04-15 10:26:56 +0800

    Add files via upload

---
+++ soft1614080902109/app/MainActivity.java
@@ -0,0 +1,13 @@
+package mylistview.itcast.cn.soft1614080902109;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
