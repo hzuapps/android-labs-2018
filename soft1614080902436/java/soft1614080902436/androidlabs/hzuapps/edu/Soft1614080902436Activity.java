7d1affef12b36d33e15e953f4397f1aac7b7b334
310341802 <310341802@qq.com>
2018-04-15 22:14:23 +0800

    #2#989

---
+++ soft1614080902436/java/soft1614080902436/androidlabs/hzuapps/edu/Soft1614080902436Activity.java
@@ -0,0 +1,14 @@
+package soft1614080902436.androidlabs.hzuapps.edu;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class Soft1614080902436Activity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_soft1614080902436);
+    }
+
+}
