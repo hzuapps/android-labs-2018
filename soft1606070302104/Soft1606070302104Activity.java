3ced6f207360ad16d5e6e863c2e30e1761e26970
lili503946540 <37757572+lili503946540@users.noreply.github.com>
2018-04-15 21:27:17 +0800

    Update Soft1606070302104Activity.java

--- soft1606070302104/Soft1606070302104Activity.java
+++ soft1606070302104/Soft1606070302104Activity.java
@@ -1,13 +1,1 @@
-package edu.hzuapps.andriodlabs.soft1606070302104;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-
-public class Soft1606070302104Activity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_soft1606070302104);
-    }
-}
