7778ccad33893dc97da4197f0006c20d0f09a67d
neptuneman <1016054954@qq.com>
2018-04-15 21:11:34 +0800

    #2 #976 WeatherForecast

---
+++ soft1614080902334/java/edu/hzuapps/androidlabs/soft1614080902334/Soft1614080902334WeatherActivity.java
@@ -0,0 +1,13 @@
+package edu.hzuapps.androidlabs.soft1614080902334;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Soft1614080902334WeatherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_soft1614080902334_weather);
+    }
+}
