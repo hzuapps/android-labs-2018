3c191b2de2c6868c8e0b9c6fccdea36b7cf0a7b4
linkzw <1624232490@qq.com>
2018-04-15 09:36:47 +0800

    #2 第二次试验

--- soft1614080902125/Soft1614080902125Activity.java
+++ soft1614080902125/Soft1614080902125Activity.java
@@ -1,5 +1,13 @@
-package edu.hzuapps.androidlabs.soft1614080902125;
+package com.example.linlex.soft1614080902125;
 
-public class Soft1614080902125Activity{
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 
+public class Soft1614080902125Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_soft1614080902125);
+    }
 }
