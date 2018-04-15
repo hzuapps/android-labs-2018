10b9c792536f60bc01f1779f250cb0237dd2e9f1
Ademzw <37769516+Ademzw@users.noreply.github.com>
2018-04-15 21:48:44 +0800

    Update Soft1614080902336.java

--- t123456/Soft1614080902336.java
+++ t123456/Soft1614080902336.java
@@ -1,6 +1,6 @@
-package edu.hzuapps.androidlabs.soft1614080902336;
-
-
-public class Soft1614080902336 {
-  
-}
+-package edu.hzuapps.androidlabs.t123456;		
+ -		
+ -// 		
+ -public class Tea123456Activity {		
+ -  		
+ -} 
