50578039183746ed6796abefe11f228f69f6b6f4
zhengzekun <37758532+zhengzekun@users.noreply.github.com>
2018-04-15 20:20:39 +0800

    #2 #918

--- soft1614080902112/Soft1614080902112Activity.java
+++ soft1614080902112/Soft1614080902112Activity.java
@@ -1,5 +1,13 @@
-package edu.hzuapps.androidlabs.soft1614080902112; 
+package com.example.a14775.soft1614080902112activity;
 
-public class Soft1614080902112Activity{ 
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 
+public class Soft1614080902112Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_soft1614080902112);
+    }
 }
