696b4cc6645f92c6e1890e9dbb30bb4f4ad17e89
HHuangF <1072987574@qq.com>
2018-04-15 19:02:14 +0800

    #2 #991 第二次实验

---
+++ soft1614080902440/second/Soft1614080902440Activity.java
@@ -0,0 +1,13 @@
+package com.example.edu.soft1614080902440;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Soft1614080902440Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_soft1614080902440);
+    }
+}
