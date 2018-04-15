2d865aca81a2f25c732a1868c5313753f29e93c6
wangguifeng <752677569@qq.com>
2018-04-15 18:51:13 +0800

    #2 #974第二次实验

--- soft1614080902344/Soft1614080902344Activity.java
+++ soft1614080902344/Soft1614080902344Activity.java
@@ -1,6 +1,12 @@
-package edu.hzuapps.androidlabs.soft1614080902344;
-
-// 123
-public class Soft1614080902344Activity {
-  
-}
+package androidlabs.hzuapps.edu.soft1614080902344;
+ import android.support.v7.app.AppCompatActivity;
+ import android.os.Bundle;
+ 
+ public class Soft1614080902344Activity extends AppCompatActivity {
+ 
+     @Override
+     protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_soft2);
+     }
+ }
