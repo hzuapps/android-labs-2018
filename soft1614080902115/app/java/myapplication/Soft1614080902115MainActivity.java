bd55ab49558a2dfbef12688d692b7ba56baf1b0a
zjy869827329 <869827329@qq.com>
2018-04-15 12:07:32 +0800

    #2 第二次实验zjy869827329

---
+++ soft1614080902115/app/java/myapplication/Soft1614080902115MainActivity.java
@@ -0,0 +1,13 @@
+package activitytest.example.com.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Soft1614080902115MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_soft1614080902115_main);
+    }
+}
