0c28c43ffd913a0ed72371f386ead0b2c0dea8cb
ss <kk>
2018-04-17 00:05:12 +0800

    #2 #1091 类似懂球帝实验二

---
+++ soft1614080902315/app/src/main/java/edu/hzuapps/androidlabs/soft1614080902315/Soft1614080902315Activity.java
@@ -0,0 +1,13 @@
+package edu.hzuapps.androidlabs.soft1614080902315;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Soft1614080902315Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.soft_1614080902315_activity);
+    }
+}
