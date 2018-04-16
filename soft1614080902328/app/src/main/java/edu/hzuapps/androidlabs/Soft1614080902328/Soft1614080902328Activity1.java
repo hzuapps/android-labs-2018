edc3123ce6868dccd39ec396aa06ffde73550d84
liangzhuoh <593510967@qq.com>
2018-04-17 00:30:49 +0800

    #2 #1006 小说阅读器实验二

---
+++ soft1614080902328/app/src/main/java/edu/hzuapps/androidlabs/Soft1614080902328/Soft1614080902328Activity1.java
@@ -0,0 +1,13 @@
+package edu.hzuapps.androidlabs.Soft1614080902328;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Soft1614080902328Activity1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.soft_1614080902328_activity1);
+    }
+}
---
+++ soft1614080902328/app/src/main/java/edu/hzuapps/androidlabs/Soft1614080902328/Soft1614080902328Activity2.java
@@ -0,0 +1,29 @@
+package edu.hzuapps.androidlabs.Soft1614080902328;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class Soft1614080902328Activity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.soft_1614080902328_activity2);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+}
