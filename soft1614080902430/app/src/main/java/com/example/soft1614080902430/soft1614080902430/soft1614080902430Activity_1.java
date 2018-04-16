b0a84f68eb0edfaf5a773c5646173cad8c13c145
Zhuangyupeng <1241910259@qq.com>
2018-04-16 23:01:37 +0800

    #2 #1025酒店订房

---
+++ soft1614080902430/app/src/main/java/com/example/soft1614080902430/soft1614080902430/soft1614080902430Activity_1.java
@@ -0,0 +1,13 @@
+package com.example.soft1614080902430.soft1614080902430;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class soft1614080902430Activity_1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_soft1614080902430_1);
+    }
+}
