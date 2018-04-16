aeacba280cb8eb17f08050ce3a818962f5a1a3fe
Zhaohongh <945033871@qq.com>
2018-04-16 13:18:21 +0800

    #2 实验二快递下单

---
+++ java/soft1614080902424kuaidixiadan/soft1614080902424kuaidixiadan.java
@@ -0,0 +1,13 @@
+package soft1614080902424.androidlabs.hzuapps.edu.soft1614080902424kuaidixiadan;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class soft1614080902424kuaidixiadan extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_soft1614080902424kuaidixiadan);
+    }
+}
