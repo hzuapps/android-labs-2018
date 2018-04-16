7aff5f1ef3b747a3cb1d22a42d2207ce08b09fcf
hzutanziyang <1194738143@qq.com>
2018-04-16 21:27:26 +0800

    #2 #938 万能遥控器

---
+++ soft1614080902227/app/src/main/java/com/example/soft1614080902227activity/Soft1614080902227Activity.java
@@ -0,0 +1,13 @@
+package com.example.soft1614080902227activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Soft1614080902227Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_soft1614080902227);
+    }
+}
