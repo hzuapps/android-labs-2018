da7c144d087c1db575bbbe92c97312ebb00b6646
xiaojiahao <1103074425@qq.com>
2018-04-17 18:02:52 +0800

    #2 #1161 词汇测试

---
+++ soft1614080902319/实验2/java/edu/hzuapps/androidlabs/Soft1614080902319Activity/MainActivity.java
@@ -0,0 +1,13 @@
+package edu.hzuapps.androidlabs.Soft1614080902319Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
