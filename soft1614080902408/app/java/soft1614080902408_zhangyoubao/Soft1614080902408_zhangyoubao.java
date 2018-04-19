1d021a983398f9ede4962e17c4760697d1d47001
wsgtl <2568271372@qq.com>
2018-04-19 13:10:03 +0800

    #2 实验二守望先锋掌游宝

--- soft1614080902408/app/java/soft1614080902408_zhangyoubao/Soft1614080902408_zhangyoubao.java
+++ soft1614080902408/app/java/soft1614080902408_zhangyoubao/Soft1614080902408_zhangyoubao.java
@@ -3,11 +3,11 @@ package soft1614080902408.androidlabs.hzuapps.edu.soft1614080902408_zhangyoubao;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class soft1614080902408 extends AppCompatActivity {
+public class Soft1614080902408_zhangyoubao extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_soft1614080902408);
+        setContentView(R.layout.activity_soft1614080902408_zhangyoubao);
     }
 }
