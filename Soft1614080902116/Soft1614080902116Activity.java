fe1ef6d3308a6426f62b08e8791d7039d88373d8
zrh116 <2849229053@qq.com>
2018-04-20 19:42:05 +0800

    #2 第二次实验

--- Soft1614080902116/Soft1614080902116Activity.java
+++ Soft1614080902116/Soft1614080902116Activity.java
@@ -1,5 +1,13 @@
-package edu.hzuapps.androidlabs.soft1614080902116;
+package com.example.can.soft1614080902116;
 
-public class Soft1614080902116Activity{
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Soft1614080902116Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
 }
-
