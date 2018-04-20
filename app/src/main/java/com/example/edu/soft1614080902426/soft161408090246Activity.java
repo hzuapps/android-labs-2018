b998d48b1fb9dc61153674d79404b21a2904cb88
linzsong <744740666@qq.com>
2018-04-20 20:39:56 +0800

    #2 #1027

---
+++ app/src/main/java/com/example/edu/soft1614080902426/soft161408090246Activity.java
@@ -0,0 +1,13 @@
+package com.example.edu.soft1614080902426;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class soft161408090246Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_soft161408090246);
+    }
+}
