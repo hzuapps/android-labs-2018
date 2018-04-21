bd4034f03b953e500a20ea9f3502c29441c4f4c9
unknown <>
2018-04-21 22:44:31 +0800

    #2 第2次实验

---
+++ soft1614080902114/app/src/main/java/edu/hzuapps/android/soft1614080902114/Soft1614080902114Activity.java
@@ -0,0 +1,13 @@
+package edu.hzuapps.android.soft1614080902114;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Soft1614080902114Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_soft1614080902114);
+    }
+}
