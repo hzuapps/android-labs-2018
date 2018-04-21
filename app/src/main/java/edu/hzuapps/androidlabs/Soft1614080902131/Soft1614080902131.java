6b1cb1dd00d41800c7851186f63df5bfdd6f7445
www.953145722@qq.com <www.953145722@qq.com>
2018-04-21 21:14:01 +0800

    1614080902131秒表

---
+++ app/src/main/java/edu/hzuapps/androidlabs/Soft1614080902131/Soft1614080902131.java
@@ -0,0 +1,13 @@
+package com.example.a1614080902131stopwatch;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Soft1614080902131 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
