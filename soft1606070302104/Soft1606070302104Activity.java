990d19f2ffafbd917a124a0a5622e6374d96c2fd
lili503946540 <503946540@qq.com>
2018-04-21 13:28:28 +0800

    #2第二次实验 纸牌24点

---
+++ soft1606070302104/Soft1606070302104Activity.java
@@ -0,0 +1,13 @@
+package edu.hzuapps.androidlabs2018.soft1606070302104;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Soft1606070302104Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_soft1606070302104);
+    }
+}
