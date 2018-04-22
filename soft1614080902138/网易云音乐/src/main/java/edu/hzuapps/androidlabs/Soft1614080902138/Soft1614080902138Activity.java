e8089bac1454ab722658439406a2869109d5a1d3
zaitao <37758457+zaitao@users.noreply.github.com>
2018-04-22 17:00:20 +0800

    Update Soft1614080902138Activity.java

--- soft1614080902138/网易云音乐/src/main/java/edu/hzuapps/androidlabs/Soft1614080902138/Soft1614080902138Activity.java
+++ soft1614080902138/网易云音乐/src/main/java/edu/hzuapps/androidlabs/Soft1614080902138/Soft1614080902138Activity.java
@@ -3,7 +3,7 @@ package com.example.hzu.myapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class Soft1614080902138Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
