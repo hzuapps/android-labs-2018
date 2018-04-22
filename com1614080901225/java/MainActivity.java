f2f07bda7e2dac2689c45aea0fa4a50c1d0c0dd2
LaineChen <897677029@qq.com>
2018-04-22 17:18:33 +0800

    #2 #1329 CampusHelp demo from LaineChen

---
+++ com1614080901225/java/MainActivity.java
@@ -0,0 +1,13 @@
+package com1614080901225.androidlabs.hzuapps.edu.campushelp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
