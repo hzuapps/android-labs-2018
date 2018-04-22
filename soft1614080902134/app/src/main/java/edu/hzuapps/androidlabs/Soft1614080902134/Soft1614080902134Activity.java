840655671c15ebd9e561df2fb4bc7e0526564ee5
lgc1999 <782830152@qq.com>
2018-04-22 10:11:45 +0800

    #2 #1303 第二次实验

---
+++ soft1614080902134/app/src/main/java/edu/hzuapps/androidlabs/Soft1614080902134/Soft1614080902134Activity.java
@@ -0,0 +1,13 @@
+package edu.hzuapps.androidlabs.Soft1614080902134;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+/**
+ * Created by hzu on 2018/4/22.
+ */
+
+public class Soft1614080902134Activity extends AppCompatActivity{
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(com.example.hzu.my_application.R.layout.activity_main);
+    }
+}
