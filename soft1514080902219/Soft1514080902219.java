cd538d9e4ce620fda73f1b666b23fab970e8e170
builtI <2508384677@qq.com>
2018-04-22 09:36:22 +0800

    #2 第二次实验

--- soft1514080902219/Soft1514080902219.java
+++ soft1514080902219/Soft1514080902219.java
@@ -1,4 +1,13 @@
-pakeage edu.hzuapps.androidlabs.soft1514080902219
-public class Soft1514080902219Activity
-{
+package edu.hzuapps.androidlabs.soft1514080902219;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Soft1514080902219 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_soft1514080902219);
+    }
 }
