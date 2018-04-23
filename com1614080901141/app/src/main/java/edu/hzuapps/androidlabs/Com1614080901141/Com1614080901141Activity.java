cc017cd58d0c54fd7f24218844a9875f855c616a
LiuYB135 <l>
2018-04-23 16:55:13 +0800

    实验二

---
+++ com1614080901141/app/src/main/java/edu/hzuapps/androidlabs/Com1614080901141/Com1614080901141Activity.java
@@ -0,0 +1,16 @@
+package edu.hzuapps.androidlabs.Com1614080901141;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+import com.example.lyblcass.firstapp.R;
+
+public class Com1614080901141Activity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_com1614080901141);
+    }
+
+}
