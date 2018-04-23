cfaf00558658d65b5f7ffe76db321886a634d79d
Demanding <540046593@qq.com>
2018-04-23 19:05:59 +0800

    #3#1032

---
+++ Soft1614080902422/java/edu/hzuapps/androidlabs/soft1614080902422/Soft1614080902422Activity_01.java
@@ -0,0 +1,23 @@
+package edu.hzuapps.androidlabs.soft1614080902422;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+
+
+public class Soft1614080902422Activity_01 extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_soft1614080902422_01);
+
+
+
+    }
+
+    }
+
