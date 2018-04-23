93acc263e2b7070608feb50cda1fb11941f4490b
1614080902117 <1045704637@qq.com>
2018-04-23 16:14:26 +0800

    #3 #881 第3次实验

--- soft1614080902117/MyApplication8/app/src/main/java/edu/hzuapps/androidlabs/soft1614080902117/myapplication/Soft1614080902137Activity_1.java
+++ soft1614080902117/MyApplication8/app/src/main/java/edu/hzuapps/androidlabs/soft1614080902117/myapplication/Soft1614080902137Activity_1.java
@@ -4,24 +4,22 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.Button;
 
-public class Soft1614080902117Activity extends AppCompatActivity {
+public class Soft1614080902137Activity_1 extends AppCompatActivity {
 
     Button button = null;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_soft1614080902117);
+        setContentView(R.layout.activity_soft1614080902137_1);
 
-        button = (Button)findViewById(R.id.button1);
-        button.setOnClickListener(new OnClickListener() {
+        button = (Button)findViewById(R.id.button2);
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent();
-                intent.setClass(Soft1614080902117Activity.this,Soft1614080902137Activity_1.class);
+                intent.setClass(Soft1614080902137Activity_1.this,Soft1614080902117Activity.class);
                 startActivity(intent);
             }
         });
