567c9a072a073350ef795087c94548bc1cc1356e
wangguifeng <752677569@qq.com>
2018-04-23 18:14:21 +0800

    #3 第三次实验

---
+++ soft1614080902344/java/edu/hzuapps/androidlabs/soft1614080902344/Soft1614080902344Activity.java
@@ -0,0 +1,30 @@
+package edu.hzuapps.androidlabs.soft1614080902344;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+public class Soft1614080902344Activity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_soft1614080902344);
+
+        final Activity thisActivity = this;
+
+        Button btnOpen = (Button) findViewById(R.id.button_open);
+        btnOpen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Soft1614080902344Activity.this, Soft1614080902344Activity2.class);
+                thisActivity.startActivity(intent);
+                //thisActivity.startActivityForResult(intent, 2016);
+            }
+        });
+
+    }
+
+}
