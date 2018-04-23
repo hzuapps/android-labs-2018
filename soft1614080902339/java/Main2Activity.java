c393c18287b7c2a17d205e7d6938652aed2782e0
Strakly <594956580@qq.com>
2018-04-23 19:13:50 +0800

    #3 #971 Notepad App code update from Strakly

---
+++ soft1614080902339/java/Main2Activity.java
@@ -0,0 +1,28 @@
+package soft1614080902339.androidlabs.hzuapps.edu.notepad;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        final Main2Activity thisActivity = this;
+
+        Button btnOpen = (Button) findViewById(R.id.button_open);
+        btnOpen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(thisActivity, MainActivity.class);
+                thisActivity.startActivity(intent);
+                //thisActivity.startActivityForResult(intent, 2016);
+            }
+        });
+    }
+}
