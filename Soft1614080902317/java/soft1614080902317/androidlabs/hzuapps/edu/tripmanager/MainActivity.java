23bb6ecf377f0b16694576a5c0d66c6eccaf0782
zhengjinbin <1033859407@qq.com>
2018-04-23 21:00:16 +0800

    #3 #985 实验三

---
+++ Soft1614080902317/java/soft1614080902317/androidlabs/hzuapps/edu/tripmanager/MainActivity.java
@@ -0,0 +1,30 @@
+package soft1614080902317.androidlabs.hzuapps.edu.tripmanager;
+
+
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = (Button)findViewById(R.id.button_open);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(MainActivity.this,Main2Activity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
