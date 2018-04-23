3583fb32779919b5868194f9725a32c65e9445be
1614080902137 <1147966681qq.com>
2018-04-23 13:10:04 +0800

    #3 #880 第三次实验

--- Soft1614080902137/app/src/main/java/edu/hzuapps/androidlabs/soft1614080902137/Soft1614080902137Activity.java
+++ Soft1614080902137/app/src/main/java/edu/hzuapps/androidlabs/soft1614080902137/Soft1614080902137Activity.java
@@ -1,13 +1,31 @@
 package edu.hzuapps.androidlabs.soft1614080902137;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 
 public class Soft1614080902137Activity extends AppCompatActivity {
+
+    Button button = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_soft1614080902137);
+
+        button = (Button)findViewById(R.id.button_open);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(Soft1614080902137Activity.this,Soft1614080902137Activity_next.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
