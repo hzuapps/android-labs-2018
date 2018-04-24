b44e90c6f8fd83e281eb12b94636eb56098b87bc
www.953145722@qq.com <www.953145722@qq.com>
2018-04-24 13:07:37 +0800

    第二次实验修改

--- soft1614080902131/app/src/main/java/soft1614080902131/androidlabs/hzuapps/edu/soft1614080902131/Soft1614080902131Activity.java
+++ soft1614080902131/app/src/main/java/soft1614080902131/androidlabs/hzuapps/edu/soft1614080902131/Soft1614080902131Activity.java
@@ -1,13 +1,13 @@
-package com.example.a1614080902131stopwatch;
+package soft1614080902131.androidlabs.hzuapps.edu.soft1614080902131;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Soft1614080902131 extends AppCompatActivity {
+public class Soft1614080902131Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_soft1614080902131);
     }
 }
