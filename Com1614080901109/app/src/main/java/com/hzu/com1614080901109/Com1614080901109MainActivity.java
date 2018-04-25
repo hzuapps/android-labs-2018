5b72ed763652788e6231adbf5f1c83fef0d55cc0
lixubin51 <2583278069@qq.com>
2018-04-25 15:47:33 +0800

    实验二

---
+++ Com1614080901109/app/src/main/java/com/hzu/com1614080901109/Com1614080901109MainActivity.java
@@ -0,0 +1,13 @@
+package com.hzu.com1614080901109;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Com1614080901109MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_com1614080901109_main);
+    }
+}
