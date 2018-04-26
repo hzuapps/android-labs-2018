ed641fa5f79f6e714ebdc7c23e72e2f40ba68eda
HHuangF <1072987574@qq.com>
2018-04-26 22:41:39 +0800

    #1 #991 第三次实

---
+++ soft1614080902440/Third/java/Soft1614080902440Activity1.java
@@ -0,0 +1,13 @@
+package edu.androidlabs.soft1614080902440;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Soft1614080902440Activity1 extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity1_soft1614080902440);
+    }
+}
