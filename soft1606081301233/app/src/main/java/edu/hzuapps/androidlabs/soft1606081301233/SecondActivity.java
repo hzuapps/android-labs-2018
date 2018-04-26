4ad7f550d8020d641639bb758c20ead69b2c6ae5
xiaoshanshanlin <1581537367@qq.com>
2018-04-26 23:22:46 +0800

    Soft1606081301233Activity.java和SecondActivity.java

---
+++ soft1606081301233/app/src/main/java/edu/hzuapps/androidlabs/soft1606081301233/SecondActivity.java
@@ -0,0 +1,13 @@
+package edu.hzuapps.androidlabs.soft1606081301233;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
--- soft1606081301233/app/src/main/java/edu/hzuapps/androidlabs/soft1606081301233/Soft1606081301233Activity.java
+++ soft1606081301233/app/src/main/java/edu/hzuapps/androidlabs/soft1606081301233/Soft1606081301233Activity.java
@@ -1,7 +1,11 @@
 package edu.hzuapps.androidlabs.soft1606081301233;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 
 public class Soft1606081301233Activity extends AppCompatActivity {
 
@@ -9,5 +13,13 @@ public class Soft1606081301233Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_soft1606081301233);
+        Button loginBtn = (Button) findViewById(R.id.btn_Login);
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setClass(Soft1606081301233Activity.this, SecondActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
