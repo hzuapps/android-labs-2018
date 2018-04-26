3924f362ecea48084208c4b1f1a5fdb8c84c8896
BoliChen <576537516qq.com>
2018-04-27 00:24:05 +0800

    #2 #1446第二次实验

---
+++ com1614080901106/app/src/main/java/com/example/hzu/com1614080901106/com1614080901106Activity.java
@@ -0,0 +1,13 @@
+package com.example.hzu.com1614080901106;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class com1614080901106Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_com1614080901106);
+    }
+}
