0d5a75c9b5267890ca13ff511f553dc10cf81ad0
xieguocheng <1078700658@qq.com>
2018-04-26 23:54:16 +0800

    实验三

---
+++ soft1614080902441/java/Soft1614080902441BackActivity.java
@@ -0,0 +1,14 @@
+package edu.hzuapps.androidlabs.Soft161408092441;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+
+public class Soft1614080902441BackActivity extends AppCompatActivity {
+
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_soft1614080902441_back);
+    }
+}
