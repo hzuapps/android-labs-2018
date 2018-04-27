8b5631a7e4873f53f5349c3022cbf0ebda46d3b9
陈梓聪 <820238114@qq.con>
2018-04-27 22:33:03 +0800

    #2 #1468 第二次实验

---
+++ com1614080901238/app/src/main/java/edu/hzuapps/androidlabs/com1614080901238/Com1614080901238.java
@@ -0,0 +1,13 @@
+package edu.hzuapps.androidlabs.com1614080901238.themostbeautifulwallpaper;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Com1614080901238 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.com1614080901238_activity);
+    }
+}
