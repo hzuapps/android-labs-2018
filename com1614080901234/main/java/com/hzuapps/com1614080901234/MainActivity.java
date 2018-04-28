e6ebe7e2c45c565e6ad932df31025bda45753b08
Tony <tony@mail.com>
2018-04-28 19:05:39 +0800

    #2 第二次实验

---
+++ com1614080901234/main/java/com/hzuapps/com1614080901234/MainActivity.java
@@ -0,0 +1,16 @@
+package com.hzuapps.com1614080901234;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * @author 王治平
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
