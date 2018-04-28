cb08379788c5aaf59a17b5806cbb00cd7842c80e
Qiuxiaocong <1065704360@qq.com>
2018-04-28 22:24:36 +0800

    #2第二次实验

---
+++ Com1614080901114/app/src/main/java/edu/hzuapps/androidlabs/Com1614080901114/Com1614080901114Activity.java
@@ -0,0 +1,13 @@
+package com.example.administrator.app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Com1614080901114 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_com1614080901114);
+    }
+}
