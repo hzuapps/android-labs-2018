6af4e774ea743ab28ab49090b0bbb9df870d9f31
GeekLee1998 <948899165@qq.com>
2018-04-28 22:53:43 +0800

    #3 #997 Third Labs

---
+++ soft1612070501311/main/java/Soft1612070501311Activity2.java
@@ -0,0 +1,18 @@
+package edu.hzuapps.androidlabs.soft1612070501311;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class Soft1612070501311Activity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+
+}
