505236993d9302ada0d6467d93239f6b999832f9
bbb1123 <37758440+bbb1123@users.noreply.github.com>
2018-05-01 23:13:39 +0800

    Add files via upload

---
+++ soft1606081301337/app/soft1606081301337MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.b.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.WindowManager;
+import android.widget.Button;
+
+public class soft1606081301337MainActivity extends AppCompatActivity {
+
+    private Button startButton = null;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+               getSupportActionBar().hide();// 隐藏ActionBar
+               getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);//remove notification bar  即全屏
+        setContentView(R.layout.activity_soft1606081301337_main);
+
+                        startButton = (Button)findViewById(R.id.button);
+               startButton.setOnClickListener(new OnClickListener(){
+
+                   @Override
+           public void onClick(View v) {
+                               Intent intent = new Intent(  soft1606081301337MainActivity.this,soft1606081301337Activity2.class);
+                             startActivity(intent);
+                            }
+       } );
+
+
+    }	     }
+
+
+
+
+
