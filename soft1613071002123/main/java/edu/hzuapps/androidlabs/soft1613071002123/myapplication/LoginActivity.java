11ecbcd68650474630a5e52bffef5f1f612abe2f
Beginner-hww <15217821903@163.com>
2018-05-02 17:13:03 +0800

    #3 实验3

--- soft1613071002123/main/java/edu/hzuapps/androidlabs/soft1613071002123/myapplication/LoginActivity.java
+++ soft1613071002123/main/java/edu/hzuapps/androidlabs/soft1613071002123/myapplication/LoginActivity.java
@@ -1,13 +1,16 @@
 package edu.hzuapps.androidlabs.soft1613071002123.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
-public class Soft1613071002123Activity extends AppCompatActivity {
+/**
+ * Created by hww on 2018/5/2.
+ */
 
+public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_soft1613071002123);
+        setContentView(R.layout.login);
     }
 }
