ffc13984661199dc992a5a4bc35f64f531d2baba
liyanghey <1501077693@qq.com>
2018-05-02 21:59:50 +0800

    #3 #1031 第三次实验

--- soft1614080902410/app/src/main/java/com/example/liyang/soft1614080902410/Soft161408092410Activity.java
+++ soft1614080902410/app/src/main/java/com/example/liyang/soft1614080902410/Soft161408092410Activity.java
@@ -2,6 +2,10 @@ package com.example.liyang.soft1614080902410;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
 
 public class Soft161408092410Activity extends AppCompatActivity {
 
@@ -9,5 +13,15 @@ public class Soft161408092410Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_soft161408092410);
+
+        final Activity thisActivity = this;
+        Button btnHome = (Button) findViewById(R.id.buttom1);
+        btnHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(thisActivity, Soft1614080902410_2.class);
+                thisActivity.startActivity(intent);
+            }
+        });
     }
 }
