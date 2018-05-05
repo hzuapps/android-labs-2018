6fcd605f442ff7331ecfbdf440eaa43b11213bfb
unknown <>
2018-05-05 10:09:27 +0800

    #3 第三次实验

--- com1614080901237/app/src/main/java/edu/hzuapps/androidlabs/Com1614080901237/Com1614080901237Activity.java
+++ com1614080901237/app/src/main/java/edu/hzuapps/androidlabs/Com1614080901237/Com1614080901237Activity.java
@@ -1,13 +1,16 @@
 package edu.hzuapps.androidlabs.Com1614080901237;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-
+import android.widget.Button;
 import com.example.myapplication.R;
+
+
 
 public class Com1614080901237Activity extends AppCompatActivity {
 
@@ -15,17 +18,23 @@ public class Com1614080901237Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_com1614080901237);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        Button button1=(Button) findViewById(R.id.tianjia);
+        Button button2=(Button) findViewById(R.id.chakan);
+        button1.setOnClickListener(new View.OnClickListener(){
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v){
+                Intent intent=new Intent(Com1614080901237Activity.this,secondActivity.class);
+                startActivity(intent);
             }
         });
+        button2.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent=new Intent(Com1614080901237Activity.this,secondActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 
 }
