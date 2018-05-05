ffef2d7e274322b196188f1687ce0bb8cbbb283d
Tmouser <825750622@qq.com>
2018-05-05 23:28:56 +0800

    #3 #1331 第三次实验

--- com1614080901101/app/src/main/java/edu/hzuapps/androidlabs/com1614080901101/Com1614080901101Activity.java
+++ com1614080901101/app/src/main/java/edu/hzuapps/androidlabs/com1614080901101/Com1614080901101Activity.java
@@ -2,11 +2,28 @@ package edu.hzuapps.androidlabs.com1614080901101;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
 
 public class Com1614080901101Activity extends AppCompatActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_com1614080901101);
+
+        Button textView = (Button) findViewById(R.id.button_open);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Com1614080901101Activity.this, Activity2.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }
