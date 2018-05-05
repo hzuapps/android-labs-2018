fea1b345788cb4fc53820556cf660b39b9e4af7c
hzulwy <1970698662@qq.com>
2018-05-06 02:11:19 +0800

    #3 第三次实验

---
+++ soft1614080902228/app/src/main/java/jiemian.java
@@ -0,0 +1,17 @@
+package edu.hzuapps.andriodlabs.soft1614080902228;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class jiemian extends Activity{
+	@Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+      
+  }
+}
