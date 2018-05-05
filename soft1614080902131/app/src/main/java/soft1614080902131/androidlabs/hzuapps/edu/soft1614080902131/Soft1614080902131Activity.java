8fab4274cc53c082ae1c9e486301cb52ee70c1c1
www.953145722@qq.com <www.953145722@qq.com>
2018-05-05 12:01:24 +0800

    第三次实验

--- soft1614080902131/app/src/main/java/soft1614080902131/androidlabs/hzuapps/edu/soft1614080902131/Soft1614080902131Activity.java
+++ soft1614080902131/app/src/main/java/soft1614080902131/androidlabs/hzuapps/edu/soft1614080902131/Soft1614080902131Activity.java
@@ -1,13 +1,27 @@
 package soft1614080902131.androidlabs.hzuapps.edu.soft1614080902131;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class Soft1614080902131Activity extends AppCompatActivity {
+
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_soft1614080902131);
+
+        imageView=(ImageView)findViewById(R.id.image_view);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(Soft1614080902131Activity.this,Soft1614080902131Activity2.class);
+                startActivity(intent);
+            }
+        });
     }
 }
