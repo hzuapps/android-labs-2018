d86872fba91c2fb94434ba6e4f4b8f3919dc691c
cjhxxz <1768861061@qq.com>
2018-05-05 17:30:21 +0800

    实验三

---
+++ com1614080901134/app/src/main/java/edu/hzuapps/androidlabs/com1614080901134/com16140809011342Activity.java
@@ -0,0 +1,13 @@
+package edu.hzuapps.androidlabs.com1614080901134;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class com16140809011342Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_com16140809011342);
+    }
+}
