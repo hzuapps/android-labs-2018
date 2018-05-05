d4aa19a0b6d0b253a7dae32a88f441d906c4f460
moxin <425420437@qq.com>
2018-05-06 01:12:40 +0800

    #第三次实验 #1478

--- com1614080901217/app/src/main/java/edu/hzuapps/androidlabs/com1614080901217/Com1614080901217.java
+++ com1614080901217/app/src/main/java/edu/hzuapps/androidlabs/com1614080901217/Com1614080901217.java
@@ -1,13 +1,24 @@
-package edu.hzuapps.androidlabs.com1614080901217
+package com.app.hzu.com1614080901217;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Com1614080901217 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.com1614080901217_activity);
+        setContentView(R.layout.activity_com1614080901217);
+        Button button=(Button)findViewById(R.id.Button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Com1614080901217.this,SecondActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
