f3d082e72a030ab929dd2f932f9ae56035e8db04
xiebaoshu <741423194@qq.com>
2018-05-05 11:51:47 +0800

    第三次实验

--- com1614080901204/app/src/main/java/com/hzu/com1614080901204/Com1614080901204MainActivity.java
+++ com1614080901204/app/src/main/java/com/hzu/com1614080901204/Com1614080901204MainActivity.java
@@ -1,13 +1,26 @@
 package com.hzu.com1614080901204;
-
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.TextView;
 public class Com1614080901204MainActivity extends AppCompatActivity {
-
+    private TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_com1614080901204_main);
+        textView=(TextView)findViewById(R.id.button);
+
+        textView.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v){
+
+                Intent intent=new Intent(Com1614080901204MainActivity.this,SecondActivity.class);
+
+                startActivity(intent);
+
+            }});
     }
 }
+
