c46bd195efe53963601d7cdd9ff0e541032b936b
imagic97 <315636696@qq.com>
2018-05-06 01:26:29 +0800

    #1515 #3 第三次实验

--- com1614080901222/app/src/main/java/edu/hzuapps/androidlabs/Com1614080901222/Com1614080901222Activity.java
+++ com1614080901222/app/src/main/java/edu/hzuapps/androidlabs/Com1614080901222/Com1614080901222Activity.java
@@ -1,7 +1,10 @@
 package edu.hzuapps.androidlabs.Com1614080901222;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.imagic.com1614080901222.R;
 
@@ -11,5 +14,13 @@ public class Com1614080901222Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_com1614080901222);
+        Button button=(Button)findViewById(R.id.Button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Com1614080901222Activity.this,SecondActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
