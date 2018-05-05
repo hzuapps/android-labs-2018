c83dec7474a98872fa3f45e88ac86b80b06b62d8
zrh116 <2849229053@qq.com>
2018-05-05 16:21:09 +0800

    第三次实验

---
+++ Soft1614080902116/app/java/soft1614080902116/Soft16140809021162.java
@@ -0,0 +1,13 @@
+package edu.huzapps.androidlabs.soft1614080902116;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Soft16140809021162 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_soft16140809021162);
+    }
+}
