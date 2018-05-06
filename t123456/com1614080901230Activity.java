1c4aadd7d9940bf7d23f40e538e6b77a03a30c50
unknown <Ademzw>
2018-05-06 17:47:14 +0800

    #1 t123456

--- t123456/com1614080901230Activity.java
+++ t123456/com1614080901230Activity.java
@@ -1,8 +1,8 @@
--package edu.hzuapps.androidlabs.com1614080901230;		
--		
--public class Com1614080901230Activity {		
-- 		
--public static void main(String args[]){		
--		
--} 		
--} 
+package edu.hzuapps.androidlabs.com1614080901230;
+
+public class Com1614080901230Activity {
+ 
+public static void main(String args[]){	
+
+}
+}
