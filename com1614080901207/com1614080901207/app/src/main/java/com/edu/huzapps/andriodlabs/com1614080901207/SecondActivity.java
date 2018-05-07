47cf5ed89796497427c3446b6a8f56191be9cf75
BlueDuper <1442966070@qq.com>
2018-05-07 22:02:03 +0800

    #3 第三次实验

---
+++ com1614080901207/com1614080901207/app/src/main/java/com/edu/huzapps/andriodlabs/com1614080901207/SecondActivity.java
@@ -0,0 +1,19 @@
+package com.edu.huzapps.andriodlabs.com1614080901207;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_com1614080901207);
+        setContentView(R.layout.activity_second);
+        setTitle("打开app");
+    }
+}
