37d5efec89f107712662797e9928a901b7493408
KaiMin8X8 <727309017@qq.com>
2018-05-07 19:44:18 +0800

    #3#968

--- Soft1614080902312/app/src/main/java/edu/hzuapps/androidlabs/Soft1614080902312/Soft1614080902312Activity.java
+++ Soft1614080902312/app/src/main/java/edu/hzuapps/androidlabs/Soft1614080902312/Soft1614080902312Activity.java
@@ -1,5 +1,7 @@
-package com.example.hzu.saolei;
+package edu.hzuapps.androidlabs.Soft1614080902312;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,6 +10,9 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+
+import edu.hzuapps.androidlabs.Soft1614080902312.*;
 
 public class Soft1614080902312Activity extends AppCompatActivity {
 
@@ -15,38 +20,19 @@ public class Soft1614080902312Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_soft1614080902312);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        final Activity thisActivity = this;
+
+        Button button = (Button) findViewById(R.id.button_open);
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent = new Intent();
+                intent.setClass(Soft1614080902312Activity.this,saolei.class);
+                thisActivity.startActivity(intent);
+
             }
         });
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_soft1614080902312, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
