efb3e8fd69a72b868c3c747ad9371dd8aaef5361
fosysink <2635469833@qq.com>
2018-05-07 12:51:27 +0800

    #2第二次实验

---
+++ Com1614080901120/app/src/main/java/com/hzu/com1614080901120/Com1614080901120MainActivity.java
@@ -0,0 +1,13 @@
+package com.hzu.com1614080901120;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Com1614080901120MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_com1614080901120_main);
+    }
+}
