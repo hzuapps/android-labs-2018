b50870968ca603c8a9c9bda4cacd769b1fa8fcce
cjhxxz <1768861061@qq.com>
2018-05-09 19:57:30 +0800

    #实验三

--- com1614080901137/app/src/main/java/edu/hzuapps/androidlabs/com1614080901137/com1614080901137activity/second_Activity.java
+++ com1614080901137/app/src/main/java/edu/hzuapps/androidlabs/com1614080901137/com1614080901137activity/second_Activity.java
@@ -3,11 +3,11 @@ package edu.hzuapps.androidlabs.com1614080901137.com1614080901137activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class com1614080901137Activity extends AppCompatActivity {
+public class second_Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_com1614080901137);
+        setContentView(R.layout.activity_second);
     }
 }
