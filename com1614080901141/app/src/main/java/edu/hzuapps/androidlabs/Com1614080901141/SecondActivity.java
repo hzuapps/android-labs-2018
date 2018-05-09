94f937852ea96f3b442d109e132bca70e8318a17
LiuYB135 <l>
2018-05-09 21:01:56 +0800

    第4次实验

--- com1614080901141/app/src/main/java/edu/hzuapps/androidlabs/Com1614080901141/SecondActivity.java
+++ com1614080901141/app/src/main/java/edu/hzuapps/androidlabs/Com1614080901141/SecondActivity.java
@@ -1,7 +1,10 @@
 package edu.hzuapps.android.labs.Com1614080901141;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.cnt.ball.R;
 
@@ -11,5 +14,14 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        Button button1 = (Button)findViewById(R.id.button_1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SecondActivity.this,ThirdActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ com1614080901141/app/src/main/java/edu/hzuapps/androidlabs/Com1614080901141/ThirdActivity.java
@@ -0,0 +1,15 @@
+package edu.hzuapps.android.labs.Com1614080901141;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.cnt.ball.R;
+
+public class ThirdActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_third);
+    }
+}
