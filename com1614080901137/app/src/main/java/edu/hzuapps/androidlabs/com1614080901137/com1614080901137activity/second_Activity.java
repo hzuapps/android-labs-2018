7a505e58c7b6766c23baef2dce1fb7847e6d6ddd
Hongms <2820455883@qq.com>
2018-05-09 20:08:56 +0800

    # 实验三

---
+++ com1614080901137/app/src/main/java/edu/hzuapps/androidlabs/com1614080901137/com1614080901137activity/second_Activity.java
@@ -0,0 +1,13 @@
+package edu.hzuapps.androidlabs.com1614080901137.com1614080901137activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class second_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
