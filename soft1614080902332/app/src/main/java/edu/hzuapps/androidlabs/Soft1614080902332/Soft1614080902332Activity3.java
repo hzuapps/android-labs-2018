4571734e35559b99b0401729e819e5784f95a595
QFJay <1037494155@qq.com>
2018-05-11 00:59:53 +0800

    #4 #1013第四次实验

--- soft1614080902332/app/src/main/java/edu/hzuapps/androidlabs/Soft1614080902332/Soft1614080902332Activity3.java
+++ soft1614080902332/app/src/main/java/edu/hzuapps/androidlabs/Soft1614080902332/Soft1614080902332Activity3.java
@@ -9,17 +9,11 @@ import android.widget.Button;
 import android.view.View.OnClickListener;
 import android.widget.ImageButton;
 
-public class Soft1614080902332Activity2 extends AppCompatActivity {
-
-
-
+public class Soft1614080902332Activity3 extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.soft_1614080902332_activity2);
-
-
-
+        setContentView(R.layout.soft_1614080902332_activity3);
 
     }
 }
