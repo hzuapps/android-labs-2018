05a3c40ebde01a76e8fa39f209ae64095ed30052
Beginner-hww <15217821903@163.com>
2018-05-10 21:24:31 +0800

    #4 第四次实验

--- soft1613071002123/main/java/edu/hzuapps/androidlabs/soft1613071002123/myapplication/LoginActivity.java
+++ soft1613071002123/main/java/edu/hzuapps/androidlabs/soft1613071002123/myapplication/LoginActivity.java
@@ -2,7 +2,6 @@ package edu.hzuapps.androidlabs.soft1613071002123.myapplication;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-
 /**
  * Created by hww on 2018/5/2.
  */
--- soft1613071002123/main/java/edu/hzuapps/androidlabs/soft1613071002123/myapplication/Soft1613071002123Activity.java
+++ soft1613071002123/main/java/edu/hzuapps/androidlabs/soft1613071002123/myapplication/Soft1613071002123Activity.java
@@ -1,20 +1,20 @@
 package edu.hzuapps.androidlabs.soft1613071002123.myapplication;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-
-import edu.hzuapps.androidlabs.soft1613071002123.myapplication.R;
 
 public class Soft1613071002123Activity extends AppCompatActivity{
 
     private Button login;
+    private Button register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_soft1613071002123);
+        //登录按钮
         login = (Button)findViewById(R.id.button_login);
         login.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -23,7 +23,14 @@ public class Soft1613071002123Activity extends AppCompatActivity{
                 startActivity(intent);
             }
         });
+        //注册按钮
+        register = (Button)findViewById(R.id.button_register);
+        register.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(Soft1613071002123Activity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
     }
-
-
 }
