45889885b3129680a6568e1231c24c0fed5785a9
Moshaojia <935833717@qq.com>
2018-05-11 15:19:34 +0800

    #4 第四次实验

--- soft1614080902405/a4/main/java/edu/hzuapps/androidlabs/soft1614080902405/soft1614080902405activityjava/Soft1614080902405Activity.java
+++ soft1614080902405/a4/main/java/edu/hzuapps/androidlabs/soft1614080902405/soft1614080902405activityjava/Soft1614080902405Activity.java
@@ -9,7 +9,7 @@ public class Soft1614080902405Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.neirong);
-
+   
 
 
 
