7d0d76a0260385edfe8eef526bb8e8cafb062124
hzulinzr <675350941@qq.com>
2018-05-11 22:30:37 +0800

    #4 #911 第四次实验+报告

--- soft1614080902212/app/src/main/java/edu/hzuapps/androidlabs/soft1614080902212/SecondActivity.java
+++ soft1614080902212/app/src/main/java/edu/hzuapps/androidlabs/soft1614080902212/SecondActivity.java
@@ -1,29 +1,40 @@
 package edu.hzuapps.androidlabs.soft1614080902212;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 public class SecondActivity extends AppCompatActivity {
 
+    private Button Button;
+    private EditText Edit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        Button=(Button)findViewById(R.id.button2);
+        Edit = (EditText)findViewById(R.id.sr1);
+        Button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                String sr = Edit.getText().toString();
+
+                if (sr.equals("")) {
+
+                    Toast.makeText(SecondActivity.this, "请输入您的时间段", Toast.LENGTH_SHORT).show();
+
+                } else {
+                    Intent intent=new Intent(SecondActivity.this,ThirdActivity.class);
+                    startActivity(intent);
+
+                }
             }
         });
     }
-
 }
---
+++ soft1614080902212/report4.md
@@ -0,0 +1,27 @@
+# 第四次实验
+
+## 1.实验目标
+
+1.根据选题要求设计界面布局及控件使用；
+2.布局xml及界面控制操作代码提交并截图；
+3.将应用运行结果截图。
+
+## 2.实验步骤
+
+1.使用其中一种布局方式，设计所选题目的界面；
+2.通过控件ID获取界面对象，执行相关操作；
+3.实现界面控件的事件处理，例如点击按钮；
+
+## 3.实验结果
+
+![](https://github.com/hzulinzr/android-labs-2018/blob/master/soft1614080902212/png4.1.png)
+
+![](https://github.com/hzulinzr/android-labs-2018/blob/master/soft1614080902212/png4.2.png)
+
+![](https://github.com/hzulinzr/android-labs-2018/blob/master/soft1614080902212/png4.3.png)
+
+![](https://github.com/hzulinzr/android-labs-2018/blob/master/soft1614080902212/png4.4.png)
+
+## 4.实验体会
+
+这次实验对我来说挺困难的，在同学的帮助下，终于完成了这次实验。了解了许多之前不懂的知识。
