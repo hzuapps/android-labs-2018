aa3328df69a496df47a9cde6c4d09c9cecd26966
HHuangF <1072987574@qq.com>
2018-05-11 14:57:07 +0800

    #4 # 991 第四次实验

---
+++ soft1614080902440/Fourth/java/Soft1614080902440Activity.java
@@ -0,0 +1,27 @@
+package edu.androidlabs.soft1614080902440;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Soft1614080902440Activity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_soft1614080902440);
+        final Activity thisActivity = this;
+
+        Button btnOpen = (Button) findViewById(R.id.button_open);
+        btnOpen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(thisActivity,Soft1614080902440Activity1.class);
+                thisActivity.startActivity(intent);
+
+            }
+        });
+    }
+}
