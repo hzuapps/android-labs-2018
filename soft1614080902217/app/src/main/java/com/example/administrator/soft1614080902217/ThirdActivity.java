4d3113d25201100cb079ec983daacbfcf5d0354b
curtain2017xxxx <1336755141@qq.com>
2018-05-12 19:44:47 +0800

    #4 #903 第四次实验

--- soft1614080902217/app/src/main/java/com/example/administrator/soft1614080902217/ThirdActivity.java
+++ soft1614080902217/app/src/main/java/com/example/administrator/soft1614080902217/ThirdActivity.java
@@ -1,7 +1,7 @@
 package com.example.administrator.soft1614080902217;
 
-import android.os.Bundle;
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -9,26 +9,23 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 
-public class SecondActivity extends AppCompatActivity {
+public class ThirdActivity extends AppCompatActivity {
 
-
-    private Button xiazai;
+    private Button bofang;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_second);
-
-        xiazai=(Button)findViewById(R.id.button_xiazai);
-        xiazai.setOnClickListener(new View.OnClickListener() {
+        setContentView(R.layout.activity_third);
+       bofang=(Button)findViewById(R.id.button_bofang);
+       bofang.setOnClickListener(new View.OnClickListener() {
                                       @Override
                                       public void onClick(View v) {
-                                          Intent intent=new Intent(SecondActivity.this,ThirdActivity.class);
+                                          Intent intent=new Intent(ThirdActivity.this,FourthActivity.class);
                                           startActivity(intent);
                                       }
                                   }
 
         );
-
     }
 
 }
