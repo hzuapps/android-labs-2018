6c00566a620721b644d6a2829d55ab85cd734001
Shinelon <Shinelon>
2018-05-12 23:19:45 +0800

    #4第四次实验

--- soft1614080902207/Soft1614080902207/main/java/hzuapps/edu/soft1614080902207/MainActivity.java
+++ soft1614080902207/Soft1614080902207/main/java/hzuapps/edu/soft1614080902207/MainActivity.java
@@ -1,8 +1,11 @@
 package hzuapps.edu.soft1614080902207;
 
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.content.Intent;
 
@@ -21,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
     }
+
+
 }
