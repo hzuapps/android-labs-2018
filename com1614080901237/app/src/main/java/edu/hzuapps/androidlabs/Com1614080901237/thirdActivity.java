72cc69ce5036cfbdf07ee3ad755766a111ca57d3
unknown <>
2018-05-12 20:06:49 +0800

    #4 第四次实验

---
+++ com1614080901237/app/src/main/java/edu/hzuapps/androidlabs/Com1614080901237/thirdActivity.java
@@ -0,0 +1,15 @@
+package edu.hzuapps.androidlabs.Com1614080901237;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.myapplication.R;
+
+public class thirdActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_third);
+    }
+}
