c851b882e0c6ce5e83ca56e6b2c7211d8560e27f
guoxianzhuang <244176448@qq.com>
2018-05-12 22:41:48 +0800

    #4 超市管理系统

--- soft1614080902208/Soft1614080902208/app/src/main/java/edu/hzuapps/androidlabs/soft1614080902208/zhigongActivity.java
+++ soft1614080902208/Soft1614080902208/app/src/main/java/edu/hzuapps/androidlabs/soft1614080902208/zhigongActivity.java
@@ -1,13 +1,13 @@
-package edu.example.soft1614080902208;
+package com.example.hzu.soft1614080902208;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Main2Activity extends AppCompatActivity {
+public class zhigongActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        setContentView(R.layout.activity_zhigong);
     }
 }
