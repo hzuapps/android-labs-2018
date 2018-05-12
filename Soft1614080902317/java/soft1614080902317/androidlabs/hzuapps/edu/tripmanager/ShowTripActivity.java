35b5dca37e30beb82b2fcdb867b15a387c75cbb1
zhengjinbin <1033859407@qq.com>
2018-05-12 23:07:21 +0800

    #4 实验四

--- Soft1614080902317/java/soft1614080902317/androidlabs/hzuapps/edu/tripmanager/ShowTripActivity.java
+++ Soft1614080902317/java/soft1614080902317/androidlabs/hzuapps/edu/tripmanager/ShowTripActivity.java
@@ -3,11 +3,11 @@ package soft1614080902317.androidlabs.hzuapps.edu.tripmanager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Soft1614080902317 extends AppCompatActivity {
+public class ShowTripActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_soft1614080902317);
+        setContentView(R.layout.activity_show_trip);
     }
 }
