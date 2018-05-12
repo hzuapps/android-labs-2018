cc6500babbfd4ad14ead2a3cf79ead6270f224c3
hzulwy <1970698662@qq.com>
2018-05-12 22:39:44 +0800

    #3 第三次实验

--- soft1614080902228/app/src/main/java/Soft1614080902228Activity.java
+++ soft1614080902228/app/src/main/java/Soft1614080902228Activity.java
@@ -1,11 +1,8 @@
 package edu.hzuapps.andriodlabs.soft1614080902228;
 
-import android.R.layout;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
-import android.text.style.BackgroundColorSpan;
-import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -15,7 +12,7 @@ public class Soft1614080902228Activity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.linearlayout);
+        setContentView(R.layout.activity_jiemian);
         Button bt=(Button)findViewById(R.id.button1);
         bt.setOnClickListener(new OnClickListener() {
  		
@@ -23,7 +20,8 @@ public class Soft1614080902228Activity extends Activity {
  		public void onClick(View v) {
  			// TODO Auto-generated method stub
  			 Intent intent=new Intent();
- 			  intent.setClass(Soft1614080902228Activity.this, jiemian.class);
+ 			  intent.setClass(Soft1614080902228Activity.this, Jiemian.class);
+ 			  Soft1614080902228Activity.this.startActivity(intent);
  		}
  	});
   }
