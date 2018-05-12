b3527aace88bdf7d54b093e8600356ff4b6bd19b
zzhuangj <37769321+zzhuangj@users.noreply.github.com>
2018-05-12 19:35:30 +0800

    Update OpenActivity.java

--- soft1614080902413/java/edu/androidlabs/soft1614080902413/OpenActivity.java
+++ soft1614080902413/java/edu/androidlabs/soft1614080902413/OpenActivity.java
@@ -1,7 +1,10 @@
-package edu.androidlabs.soft1614080902413;
+  package edu.androidlabs.soft1614080902413;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class OpenActivity extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class OpenActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_open);
         setTitle("皮皮钟已打开");
+        Button button1 = (Button)findViewById(R.id.btn1);/*c将layout中创建的button关联起来*/
+        button1.setOnClickListener(button_listener);
+        Button button2 = (Button)findViewById(R.id.btn2);/*c将layout中创建的button关联起来*/
+        button2.setOnClickListener(button_listener);
+        Button button3 = (Button)findViewById(R.id.btn3);/*c将layout中创建的button关联起来*/
+        button2.setOnClickListener(button_listener);
     }
+    private Button.OnClickListener button_listener = new Button.OnClickListener(){     //创建button listener
+        public void onClick(View v){
+
+        }
+    };
 }
