a00e0be480740f266dc4847462714109b11b4336
unknown <>
2018-05-12 21:49:25 +0800

    #3 #1519 第3次实验

---
+++ Com1614080901210/app/src/main/java/edu/hzuapps/androidlabs/Com1614080901210/Com1614080901210Activity/java/secondActivity.java
@@ -0,0 +1,13 @@
+package edu.hzuapps.androidlabs.Com1614080901210.Com1614080901210Activity.java;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class secondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
