755edb346b9b1aa0ccb531c0feb8621a140e3648
zrh116 <37758517+zrh116@users.noreply.github.com>
2018-05-12 11:32:35 +0800

    Add files via upload

---
+++ Soft1614080902116/Soft1614080902116Activity.java
@@ -0,0 +1,13 @@
+package com.example.can.soft1614080902116;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Soft1614080902116Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
