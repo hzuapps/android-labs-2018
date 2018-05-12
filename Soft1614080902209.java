d5300528f359d776460635b11122d55f756fa8d2
zzs-zhangxiaoxiao <37758934+zzs-zhangxiaoxiao@users.noreply.github.com>
2018-05-12 21:25:21 +0800

    Add files via upload

---
+++ Soft1614080902209.java
@@ -0,0 +1,24 @@
+package edu.hzuapps.androidlabs.soft1614080902209;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class Soft1614080902209 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_soft1614080902209);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+    }
+
+}
