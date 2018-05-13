37870b4f672b5d5e40a4738dcbd05d808eb0af68
WLIYUN <2531233108@qq.com>
2018-05-13 21:06:04 +0800

    #4 the fourth lab

--- soft1614080902428/java/Soft1614080902428activityMain2Activity.java
+++ soft1614080902428/java/Soft1614080902428activityMain2Activity.java
@@ -16,6 +16,23 @@ public class Soft1614080902428activityMain2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.soft1614080902428activity_main2);
+        Button button1=(Button) findViewById( R.id.btn1_editnote);
+        button1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(Soft1614080902428activityMain2Activity.this,chakan.class);
+                startActivity(intent);
+            }
+        });
+        Button button2=(Button)findViewById(R.id.btn2_editnote);
+        button2.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent();
+                intent.setClass(Soft1614080902428activityMain2Activity.this,xiugai.class);
+                startActivity(intent);
+            }
+        });
     }
 
 }
---
+++ soft1614080902428/java/chakan.java
@@ -0,0 +1,14 @@
+package com.hzu.edu.myapplication;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class chakan extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chakan);
+    }
+
+}
