e55ec5b85da482ec6653b076508297b2007d2fde
as6296463 <37761817+as6296463@users.noreply.github.com>
2018-05-13 11:52:58 +0800

    Update com1614080901219activity1.java

--- com1614080901219/app3/src/main/java/edu/hzuapps/androidlabs/com1614080901219/com1614080901219activity1.java
+++ com1614080901219/app3/src/main/java/edu/hzuapps/androidlabs/com1614080901219/com1614080901219activity1.java
@@ -8,7 +8,7 @@ import android.view.View;
 import android.widget.Button;
 import android.view.View.OnClickListener;
 
-public class com1614080901219activity1 extends Activity {
+public class com1614080901219activity1 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
