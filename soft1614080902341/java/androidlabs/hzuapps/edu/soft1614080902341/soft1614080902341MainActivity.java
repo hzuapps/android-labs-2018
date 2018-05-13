c764b1c593e0bad42c7610d7402b9176b59140c2
name <email@baidu.com>
2018-05-13 14:25:13 +0800

    #4 #1037 实验4

--- soft1614080902341/java/androidlabs/hzuapps/edu/soft1614080902341/soft1614080902341MainActivity.java
+++ soft1614080902341/java/androidlabs/hzuapps/edu/soft1614080902341/soft1614080902341MainActivity.java
@@ -14,42 +14,66 @@ public class soft1614080902341MainActivity extends ActionBarActivity {
     private Button myBtn_tow;
     private Button myBtn_thr;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_soft1614080902341_main);
-        myBtn_one =(Button) findViewById(R.id.but_one);
-        myBtn_tow =(Button) findViewById(R.id.but_tow);
+        myBtn_one = (Button) findViewById(R.id.but_one);
+        myBtn_tow = (Button) findViewById(R.id.but_tow);
         myBtn_thr = (Button) findViewById(R.id.but_thr);
-        myBtn_one.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // 另一个Activity的完整名称 = edu.androidlabs.soft123456(包名).Soft123456Activity(类名)
-                Intent intent=new Intent();
-                intent.setClass(soft1614080902341MainActivity.this, jiandaoMainActivity.class); //设置跳转的Activity
-                soft1614080902341MainActivity.this.startActivity(intent);
-            }
-        });
-        myBtn_tow.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // 另一个Activity的完整名称 = edu.androidlabs.soft123456(包名).Soft123456Activity(类名)
-                Intent intent=new Intent();
-                intent.setClass(soft1614080902341MainActivity.this, shitouMainActivity.class); //设置跳转的Activity
-                soft1614080902341MainActivity.this.startActivity(intent);
-            }
-        });
-        myBtn_thr.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // 另一个Activity的完整名称 = edu.androidlabs.soft123456(包名).Soft123456Activity(类名)
-                Intent intent=new Intent();
-                intent.setClass(soft1614080902341MainActivity.this, buMainActivity.class); //设置跳转的Activity
-                soft1614080902341MainActivity.this.startActivity(intent);
-            }
-        });
-
+        //通过实现onclicklistener接口的类的对象
+        myBtn_one.setOnClickListener(new MainActivity());
+        myBtn_tow.setOnClickListener(new MainActivity());
+        myBtn_thr.setOnClickListener(new MainActivity());
     }
+//        myBtn_one.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                // 另一个Activity的完整名称 = edu.androidlabs.soft123456(包名).Soft123456Activity(类名)
+//                Intent intent=new Intent();
+//                intent.setClass(soft1614080902341MainActivity.this, jiandaoMainActivity.class); //设置跳转的Activity
+//                soft1614080902341MainActivity.this.startActivity(intent);
+//            }
+//        });
+//        myBtn_tow.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                // 另一个Activity的完整名称 = edu.androidlabs.soft123456(包名).Soft123456Activity(类名)
+//                Intent intent=new Intent();
+//                intent.setClass(soft1614080902341MainActivity.this, shitouMainActivity.class); //设置跳转的Activity
+//                soft1614080902341MainActivity.this.startActivity(intent);
+//            }
+//        });
+//        myBtn_thr.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                // 另一个Activity的完整名称 = edu.androidlabs.soft123456(包名).Soft123456Activity(类名)
+//                Intent intent=new Intent();
+//                intent.setClass(soft1614080902341MainActivity.this, buMainActivity.class); //设置跳转的Activity
+//                soft1614080902341MainActivity.this.startActivity(intent);
+//            }
+//        });
+        private class MainActivity implements View.OnClickListener {
+            @Override
+            public void onClick(View v){
+                Intent intent=new Intent();
+                switch (v.getId()){
+                    case R.id.but_one:
+                        intent.setClass(soft1614080902341MainActivity.this, jiandaoMainActivity.class); //设置跳转的Activity
+                        soft1614080902341MainActivity.this.startActivity(intent);
+                        break;
+                    case R.id.but_tow:
+                        intent.setClass(soft1614080902341MainActivity.this, shitouMainActivity.class); //设置跳转的Activity
+                        soft1614080902341MainActivity.this.startActivity(intent);
+                        break;
+                    case R.id.but_thr:
+                        intent.setClass(soft1614080902341MainActivity.this, buMainActivity.class); //设置跳转的Activity
+                        soft1614080902341MainActivity.this.startActivity(intent);
+                        break;
+                }
+            }
+        }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
