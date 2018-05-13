fbf3bcf20dc45336dfc4ff2cf2395483c80f262b
shuaidebeirenpi <1729882360@qq.com>
2018-05-13 22:38:15 +0800

    #4 #1453 第四次实验+报告

---
+++ com1614080901213/app/src/main/java/edu/hzuapps/androidlabs/com1614080901213/FourActivity.java
@@ -0,0 +1,23 @@
+package edu.hzuapps.androidlabs.com1614080901213;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+
+public class FourActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_four);
+
+    }
+
+}
--- com1614080901213/app/src/main/java/edu/hzuapps/androidlabs/com1614080901213/SecondActivity.java
+++ com1614080901213/app/src/main/java/edu/hzuapps/androidlabs/com1614080901213/SecondActivity.java
@@ -1,29 +1,57 @@
 package edu.hzuapps.androidlabs.com1614080901213;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class SecondActivity extends AppCompatActivity {
+
+    private ImageButton pai1, pai2, pai3;
+    private Button mImage01;
+    private Button mImage02;
+    private Button mImage03;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        pai1 = (ImageButton) findViewById(R.id.mImage01);
+        pai2 = (ImageButton) findViewById(R.id.mImage02);
+        pai3 = (ImageButton) findViewById(R.id.mImage03);
+        pai1.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                Intent intent = new Intent(SecondActivity.this, ThreeActivity.class);
+
+                startActivity(intent);
             }
         });
+
+        pai2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SecondActivity.this, FourActivity.class);
+
+                startActivity(intent);
+            }
+        });
+
+        pai3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SecondActivity.this, FourActivity.class);
+
+                startActivity(intent);
+            }
+        });
+
     }
 
 }
