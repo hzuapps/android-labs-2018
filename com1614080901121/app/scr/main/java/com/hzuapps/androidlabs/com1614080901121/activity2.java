dfbd1619c1337454fa36ebd6ee4d8b1e6b857bf8
Nickum <2523194054@qq.com>
2018-05-13 16:34:06 +0800

    #3 #1339第三次实验修改

---
+++ com1614080901121/app/scr/main/java/com/hzuapps/androidlabs/com1614080901121/activity2.java
@@ -0,0 +1,12 @@
+package edu.hzuapps.androidlabs.com1614080901121;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Activity2 extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity2);
+    }
+}
