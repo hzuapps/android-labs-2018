d6bcf824aee2ded4cd7f35618b6f6c4bd2e00564
CAIYB <463700371@qq.com>
2018-05-14 10:44:05 +0800

    实验三

--- com1614080901103/app/src/main/java/androidlabs/hzuapps/edu/com1614080901103/Com1614080901103Activity.java
+++ com1614080901103/app/src/main/java/androidlabs/hzuapps/edu/com1614080901103/Com1614080901103Activity.java
@@ -4,21 +4,32 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class Com1614080901103Activity extends AppCompatActivity {
 
+    private ImageView imageView;
+    private Button newbutton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-        TextView textView;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_com1614080901103);
 
-        textView=(TextView)findViewById(R.id.textview_01);
-        textView.setOnClickListener(new View.OnClickListener() {
+        imageView = (ImageView) findViewById(R.id.textview_01);
+        imageView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Intent intent=new Intent(Com1614080901103Activity.this,SecondActivity.class);
+                startActivity(intent);
+            }
+        });
+        newbutton=(Button) findViewById(R.id.button);
+        newbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
                 Intent intent=new Intent(Com1614080901103Activity.this,SecondActivity.class);
                 startActivity(intent);
             }
