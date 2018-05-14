3c64802d3cde6a11bc4d46d82ebd8810941c6f1e
mvp-szf <37758616+mvp-szf@users.noreply.github.com>
2018-05-14 22:00:54 +0800

    Add files via upload

---
+++ soft1614080902238/Soft614080902238b/app/src/main/java/edu/hzuapps/androidlabs/soft1614080902238/SecondActivity.java
@@ -0,0 +1,24 @@
+package edu.hzuapps.androidlabs.soft1614080902238;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.app.Activity;
+import android.widget.TextView;
+import android.R.layout;
+public class SecondActivity extends Activity {
+
+    public void onCreate(Bundle SavedInstanceState ){
+        super.onCreate(SavedInstanceState);
+        this.setContentView(R.layout.activity_second);
+
+
+
+
+    }
+
+
+}
