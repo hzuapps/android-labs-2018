e00c5451a46a54d317a9f01b0939634a3235dd40
ccxm <1532917281@qq.com>
2018-05-15 21:29:57 +0800

    第三次实验

---
+++ soft1614080902419/app/src/main/java/edu/hzuapps/androidlabs/Soft1614080902419/Second_Activity.java
@@ -0,0 +1,13 @@
+package edu.hzuapps.androidlabs.Soft1614080902419;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Second_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_activity);
+    }
+}
