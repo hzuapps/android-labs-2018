381d9329c9b9b524a0babeb9e90be2857341e625
Administrator <Administrator@SC-201801040210>
2018-05-17 17:31:17 +0800

    #4  4次实验

--- Com1614080901142/app/src/main/java/edu/hzuapps/androidlabs/com1614080901142/com_1614080901142Activity.java
+++ Com1614080901142/app/src/main/java/edu/hzuapps/androidlabs/com1614080901142/com_1614080901142Activity.java
@@ -3,11 +3,11 @@ package edu.hzuapps.androidlabs.com1614080901142;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class com1614080901142Activity extends AppCompatActivity {
+public class com_1614080901142Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_com1614080901142);
+        setContentView(R.layout.activity_com_1614080901142);
     }
 }
