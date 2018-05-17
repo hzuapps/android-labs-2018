cd6c52e8a223fe7b163eeb1bebe43e87f82d5c27
deasyful <253933434.com>
2018-05-17 23:44:24 +0800

    #5 #927 第五次实验

--- soft1614080902206/app/src/main/java/com/example/asus/soft1614080902206/MainActivity.java
+++ soft1614080902206/app/src/main/java/com/example/asus/soft1614080902206/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.asus.soft1614080902206;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,18 +11,12 @@ import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
     public final static String EXTRA_MESSAGE = "com.example.asus.soft1614080902206.MESSAGE";
-
-
-
     protected void onCreate(Bundle savedInstanceState) {
         String str1="";
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         final Activity thisActivity = this;
-
         Button btnOpen = (Button) findViewById(R.id.button_open);
-
-
               btnOpen.setOnClickListener(new View.OnClickListener() {
 
 
@@ -29,10 +24,11 @@ public class MainActivity extends AppCompatActivity {
 
            public void onClick(View view) {
                                Intent intent = new Intent(thisActivity, message.class);
-
                                thisActivity.startActivity(intent);
                                   }
        });
 
     }
+
+
 }
