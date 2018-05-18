3b56feb4f1f4fc16c08c452ab88e4f7dae71e476
haoy1220 <30833151+haoy1220@users.noreply.github.com>
2018-05-18 22:41:02 +0800

    #3 #11 实验4

---
+++ Soft1614080902145/TIM/src/main/java/com/example/hao/soft1614080902145/Contact_Activity.java
@@ -0,0 +1,13 @@
+package com.example.hao.soft1614080902145;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Contact_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contact_);
+    }
+}
