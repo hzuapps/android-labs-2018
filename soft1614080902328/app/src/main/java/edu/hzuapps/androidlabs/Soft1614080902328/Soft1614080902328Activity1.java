cc76476b2a7d4bb48f145e9ee2c764f4a466ef0b
liangzhuoh <593510967@qq.com>
2018-05-18 23:50:07 +0800

    #5 #1006 实验五

--- soft1614080902328/app/src/main/java/edu/hzuapps/androidlabs/Soft1614080902328/Soft1614080902328Activity1.java
+++ soft1614080902328/app/src/main/java/edu/hzuapps/androidlabs/Soft1614080902328/Soft1614080902328Activity1.java
@@ -1,27 +1,72 @@
 package edu.hzuapps.androidlabs.Soft1614080902328;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Soft1614080902328Activity1 extends AppCompatActivity {
+    private EditText mEditTextContent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.soft_1614080902328_activity1);
-        Button button1=(Button)findViewById(R.id.button_2);
-        button1.setOnClickListener(new View.OnClickListener(){
+        mEditTextContent = (EditText) findViewById(R.id.et_password);
+        Button button1 = (Button) findViewById(R.id.button_2);
+        button1.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
-                Intent intent=new Intent(Soft1614080902328Activity1.this,Soft1614080902328Activity2.class);
-                startActivity(intent);
+            public void onClick(View v) {
+
+                //向内部存储写入数据
+                saveContent();
+                Toast.makeText(Soft1614080902328Activity1.this, "saved!", Toast.LENGTH_SHORT).show();
+
             }
         });
+
     }
+    private void saveContent() {
+        FileOutputStream fos = null;
+        String content = mEditTextContent.getText().toString();
+
+        try {
 
 
+            //通过该方法向内部存储中写入数据，文件名为edit_data.txt，模式为MODE_PRIVATE，表示该文件操作权限为文本应用，另一个常用的权限MODE_APPEND表示在文件末尾添加内容
+            fos = this.openFileOutput("edit_data.txt", MODE_PRIVATE);
+            fos.write(content.getBytes());
+            fos.flush();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (fos != null) {
+                try {
+                    fos.close();
 
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }
+
+    }
 
 }
