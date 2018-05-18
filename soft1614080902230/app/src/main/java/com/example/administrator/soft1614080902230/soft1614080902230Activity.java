9b292a0199682f92845de01e009558188ec2cec2
laiguoqiang <981843817@qq.com>
2018-05-18 17:50:05 +0800

    #4 #931 第四次实验

--- soft1614080902230/app/src/main/java/com/example/administrator/soft1614080902230/soft1614080902230Activity.java
+++ soft1614080902230/app/src/main/java/com/example/administrator/soft1614080902230/soft1614080902230Activity.java
@@ -4,22 +4,22 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.*;
 import android.widget.TextView;
-
 import org.w3c.dom.Text;
 
 public class soft1614080902230Activity extends AppCompatActivity {
 
-    private TextView textView;
+    private Button Button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_soft1614080902230);
 
-        textView=(TextView)findViewById(R.id.textview_01);
-
-        textView.setOnClickListener(new View.OnClickListener() {
+        Button=(Button)findViewById(R.id.button);
+        Button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent=new Intent(soft1614080902230Activity.this,secondActivity.class);
