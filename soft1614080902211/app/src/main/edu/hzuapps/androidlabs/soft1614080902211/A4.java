07ca904ddc6655a0ddfa649cbdf43790c8c4b4a7
LinCJ1998 <37758454+LinCJ1998@users.noreply.github.com>
2018-05-19 17:52:55 +0800

    #4 第四次实验

---
+++ soft1614080902211/app/src/main/edu/hzuapps/androidlabs/soft1614080902211/A4.java
@@ -0,0 +1,13 @@
+package edu.hzuapps.androidlabs.soft1614080902211;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class A4 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_a4);
+    }
+}
