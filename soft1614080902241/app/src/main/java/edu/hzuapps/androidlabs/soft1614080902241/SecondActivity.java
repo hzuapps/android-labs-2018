766f66a299e1f737cd80e010469dd4de97f6cc0d
LuoJKL <1226848551@qq.com>
2018-05-19 17:58:21 +0800

    #4 #921 第四次实验

--- soft1614080902241/app/src/main/java/edu/hzuapps/androidlabs/soft1614080902241/SecondActivity.java
+++ soft1614080902241/app/src/main/java/edu/hzuapps/androidlabs/soft1614080902241/SecondActivity.java
@@ -1,27 +1,28 @@
 package edu.hzuapps.androidlabs.soft1614080902241;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ImageButton;
 
 public class SecondActivity extends AppCompatActivity {
+
+    private ImageButton button1_2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        button1_2=(ImageButton) findViewById(R.id.imagebutton_01_02);
+
+        button1_2.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                Intent intent1_2=new Intent(SecondActivity.this,thirdActivity.class);
+                startActivity(intent1_2);
             }
         });
     }
