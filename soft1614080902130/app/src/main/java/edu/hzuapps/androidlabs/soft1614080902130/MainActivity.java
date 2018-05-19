6447c814ecb0904858b01311e249d74c881d1096
Rvelamen <13126348842@163.com>
2018-05-20 00:04:14 +0800

    #3#937#第三次实验

---
+++ soft1614080902130/app/src/main/java/edu/hzuapps/androidlabs/soft1614080902130/MainActivity.java
@@ -0,0 +1,24 @@
+package edu.hzuapps.androidlabs.soft1614080902130;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button1=(Button)findViewById(R.id.button_1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,SecondActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
