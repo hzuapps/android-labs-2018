6559346a8ce8347cdfd682acdf90680700fa9db0
lhyyhl <3132757812@qq.com>
2018-05-19 20:25:07 +0800

    #4

---
+++ soft1614080902304/main/java/edu/hzuapps/androidlabs/soft1614080902304/soft1614080902304/eventActivity.java
@@ -0,0 +1,16 @@
+package edu.hzuapps.androidlabs.soft1614080902304.soft1614080902304;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by 李惠怡 on 2018/5/18.
+ */
+
+public class eventActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.event_layout);
+    }
+}
