d86dd78e04270acb3414c33516b639e56a492bde
lhyyhl <3132757812@qq.com>
2018-05-20 02:32:45 +0800

    #5 #diwu

--- soft1614080902304/main/java/edu/hzuapps/androidlabs/soft1614080902304/soft1614080902304/eventActivity.java
+++ soft1614080902304/main/java/edu/hzuapps/androidlabs/soft1614080902304/soft1614080902304/eventActivity.java
@@ -1,7 +1,10 @@
 package edu.hzuapps.androidlabs.soft1614080902304.soft1614080902304;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.LinearLayout;
 
 /**
  * Created by 李惠怡 on 2018/5/18.
@@ -12,5 +15,13 @@ public class eventActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.event_layout);
+        LinearLayout important_emergency=(LinearLayout) findViewById(R.id.important_emergency);
+        important_emergency.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(eventActivity.this,FileStorageActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
