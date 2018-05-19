ba9cc1b3ac5c4d588bace045530411c39afff365
TheZhangh <405667183@qq.com>
2018-05-19 11:55:14 +0800

    #3 #2034 for com

---
+++ com1614080901145/app/src/main/java/com/example/zhang/com1614080901145/Com_1614080901145Activity.java
@@ -0,0 +1,17 @@
+package com.example.zhang.apps;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class Com_1614080901145Activity extends AppCompatActivity{
+    @Override
+   protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_com_1614080901145);
+
+    }
+
+}
