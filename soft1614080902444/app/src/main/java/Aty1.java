39dfdcb1ce9e4b931f9804cbf1dbca984342dafc
linxin666 <3467967574@qq.com>
2018-05-19 17:15:15 +0800

    #3 第三次实验

---
+++ soft1614080902444/app/src/main/java/Aty1.java
@@ -0,0 +1,15 @@
+package edu.hzuapps.androidlabs.soft1614080902444;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+
+public class Aty1 extends AppCompatActivity {
+    private Button btnStartAty1;
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.aty1);
+
+
+    }
+}
