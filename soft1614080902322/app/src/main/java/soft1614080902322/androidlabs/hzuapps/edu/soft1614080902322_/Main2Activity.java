5c99eafd5ecc529c28b2c2bb4f7dc8e50e95ce91
zhongtf <1062915965@qq.com>
2018-05-20 12:26:52 +0800

    #5 #1005

--- soft1614080902322/app/src/main/java/soft1614080902322/androidlabs/hzuapps/edu/soft1614080902322_/Main2Activity.java
+++ soft1614080902322/app/src/main/java/soft1614080902322/androidlabs/hzuapps/edu/soft1614080902322_/Main2Activity.java
@@ -4,6 +4,7 @@ import android.content.ContextWrapper;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Button;
@@ -14,10 +15,15 @@ import java.io.BufferedWriter;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
+import java.io.*;
+import java.util.*;
 
 public class Main2Activity extends AppCompatActivity {
     private TextView textView;
-    @Override
+
+    public static final String DIRECTORY = "demo";
+    public static final String FILENAME = "file_demo.txt";
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
@@ -54,6 +60,52 @@ public class Main2Activity extends AppCompatActivity {
             }catch (IOException e){
                 e.printStackTrace();
             }
+        }
+        File dir = this.getFilesDir();
+        File file = new File(dir, FILENAME);
+
+
+        if (file.exists()) {
+            file.canRead();
+            file.canWrite();
+            file.canExecute();
+
+            file.getFreeSpace();
+            file.getTotalSpace();
+        }
+
+        FileOutputStream fos = null;
+
+        try {
+            fos = openFileOutput(FILENAME, MODE_PRIVATE);
+            fos.write(text.getBytes());
+            fos.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                fos.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        FileReader reader = null;
+
+        try {
+            reader = new FileReader(file.getAbsoluteFile());
+            BufferedReader bReader = new BufferedReader(reader);
+            String line = bReader.readLine();
+            Log.v("","读取的文件："+line);
+            bReader.close();
+            reader.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         }
     }
 }
