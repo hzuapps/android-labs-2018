df921fea6af896ace877ce602d7ca81cb4c9720d
BFFat <562804497@qq.com>
2018-05-20 14:49:14 +0800

    #4 #905“
    git commit -m#4

---
+++ soft1614080902222/app/src/main/java/edu/huzapps/androidlabs/soft1614080902222/Main5Activity.java
@@ -0,0 +1,19 @@
+package edu.huzapps.androidlabs.soft1614080902222;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class Main5Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main5);
+
+    }
+
+}
