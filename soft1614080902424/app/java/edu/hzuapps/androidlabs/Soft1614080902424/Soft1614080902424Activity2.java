4c227f34b4ed0b316d5dd227cb12af72b9f41bd4
Zhaohongh <945033871@qq.com>
2018-05-20 13:49:48 +0800

    #5 #1017快递下单数据存储和显示

--- soft1614080902424/app/java/edu/hzuapps/androidlabs/Soft1614080902424/Soft1614080902424Activity2.java
+++ soft1614080902424/app/java/edu/hzuapps/androidlabs/Soft1614080902424/Soft1614080902424Activity2.java
@@ -5,14 +5,24 @@ import android.os.Bundle;
 import android.content.Intent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class Soft1614080902424Activity2 extends AppCompatActivity{
-
+    private Button mButtonDisplay;
+    private Spinner mEditTextContent1;
+    private EditText mEditTextContent5;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.soft_1614080902424_activity2);
         Button send = (Button) findViewById(R.id.send);
+        mEditTextContent1 = (Spinner) findViewById(R.id.kg);
 
        send.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
@@ -40,6 +50,47 @@ public class Soft1614080902424Activity2 extends AppCompatActivity{
             }
         });
 
+        mButtonDisplay = (Button) findViewById(R.id.submit);
+        mButtonDisplay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //从内部存储读取数据
+                String s = getContent();
+                Toast.makeText(Soft1614080902424Activity2.this, s, Toast.LENGTH_SHORT).show();
+
+            }
+        });
 
     }
+    private String getContent() {
+        String s = null;
+        FileInputStream fis = null;
+        try {
+            //通过该方法从内部存储中的edit_data.txt文件中读取数据
+            fis = this.openFileInput("edit_data.txt");
+            int len = 0;
+            byte[] buf = new byte[1024];
+            while ((len = fis.read(buf)) != -1) {
+                s = new String(buf, 0, len, "UTF-8");
+
+            }
+
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (fis != null) {
+                try {
+                    fis.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return s;
+    }
+
 }
