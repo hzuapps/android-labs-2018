8811673c35d4e9cf1119ef76bad8b902ef241ef2
zjy869827329 <869827329@qq.com>
2018-05-20 11:46:53 +0800

    修改

--- soft1614080902115/app/java/edu/hzuapps/androidlabs/soft1614080902115/myapplication/Soft1614080902115MainActivity.java
+++ soft1614080902115/app/java/edu/hzuapps/androidlabs/soft1614080902115/myapplication/Soft1614080902115MainActivity.java
@@ -1,4 +1,4 @@
-package edu.androidlabs.soft1614080902115.myapplication;
+package edu.hzuapps.androidlabs.soft1614080902115.myapplication;
 
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
