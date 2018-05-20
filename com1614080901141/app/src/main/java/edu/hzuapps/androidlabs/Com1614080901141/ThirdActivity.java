5be9dcf3dfb0c57777929bb1753af43905a288ec
LiuYB135 <l>
2018-05-20 13:35:42 +0800

    #5 #1343 第五次实验

--- com1614080901141/app/src/main/java/edu/hzuapps/androidlabs/Com1614080901141/ThirdActivity.java
+++ com1614080901141/app/src/main/java/edu/hzuapps/androidlabs/Com1614080901141/ThirdActivity.java
@@ -1,7 +1,10 @@
 package edu.hzuapps.android.labs.Com1614080901141;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.cnt.ball.R;
 
@@ -11,5 +14,13 @@ public class ThirdActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_third);
+        Button button3 = (Button)findViewById(R.id.button_2);
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ThirdActivity.this,FourActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
