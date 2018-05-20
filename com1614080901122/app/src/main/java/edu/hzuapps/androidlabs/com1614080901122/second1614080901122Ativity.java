83c6d57dba85444e24106c8a7b2f32b35a1e3857
unknown <>
2018-05-20 11:33:38 +0800

    #4第4次实验

--- com1614080901122/app/src/main/java/edu/hzuapps/androidlabs/com1614080901122/second1614080901122Ativity.java
+++ com1614080901122/app/src/main/java/edu/hzuapps/androidlabs/com1614080901122/second1614080901122Ativity.java
@@ -1,11 +1,13 @@
 package edu.hzuapps.androidlabs.com1614080901122;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class second1614080901122Ativity extends AppCompatActivity {
 
@@ -24,6 +26,7 @@ public class second1614080901122Ativity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
     }
 
 }
