a2fb053f28d15608beeab34d303a17dbeec835aa
Edison-Yan <214746977@qq.com>
2018-05-21 22:48:57 +0800

    #2 第二次实验

---
+++ soft1614080902210/app/src/main/java/edu/hzuapps/androidlabs/soft1614080902210/soft1614080902210.java
@@ -0,0 +1,22 @@
+package edu.hzuapps.androidlabs.soft1614080902210;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class soft1614080902210 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_soft1614080902210);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+    }
+
