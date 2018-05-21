39d12cd4ea59b8bbc03fcd41559442e34e0ce0b8
Qiujialin <78973190@qq.com>
2018-05-21 22:28:21 +0800

    #5 #1015 第五次实验+实验报告

--- soft1614080902407/java/edu/hzuapps/androidlabs/soft1614080902407/SecondActivity.java
+++ soft1614080902407/java/edu/hzuapps/androidlabs/soft1614080902407/SecondActivity.java
@@ -1,5 +1,13 @@
 package edu.hzuapps.androidlabs.soft1614080902407;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.lang.String;
 import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
@@ -9,10 +17,9 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
-
+    private EditText editText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +27,9 @@ public class SecondActivity extends AppCompatActivity {
 
         final Activity thisActivity = this;
         Button btnBack = (Button)findViewById(R.id.gback);
+        Button btnJoin= (Button)findViewById(R.id.btnJoin);
+        editText = (EditText)findViewById(R.id.edittext);
+
         btnBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -27,5 +37,63 @@ public class SecondActivity extends AppCompatActivity {
                 thisActivity.startActivity(intent);
             }
         });
+        btnJoin.setOnClickListener(new View.OnClickListener() {//从文件读取内容
+            @Override
+            public void onClick(View v) {
+                String inputText = load();
+                editText.setText(inputText);
+                editText.setSelection(inputText.length());
+            }
+        });
+    }
+    protected void onDestroy() {                            //存储文件
+
+        super.onDestroy();
+        String  inputText = editText.getText().toString();
+        save(inputText);
+    }
+    public void save(String inputText){
+        FileOutputStream out;
+        BufferedWriter  writer = null;
+        try {
+            out = openFileOutput("data", MODE_PRIVATE);
+            writer = new BufferedWriter(new OutputStreamWriter(out));
+            writer.write(inputText);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if(writer!=null){
+                    writer.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+    public String load(){                                           //读取文件
+        FileInputStream in =null;
+        BufferedReader reader = null;
+        StringBuilder content = new StringBuilder();
+        try {
+            in = openFileInput("data");
+            reader = new BufferedReader(new InputStreamReader(in));
+            String line = "";
+            while ((line=reader.readLine())!= null){
+                content.append(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            if(reader!=null){
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return content.toString();
     }
 }
