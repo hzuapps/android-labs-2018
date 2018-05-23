c7b190a1aa6f43cfd159ea2298b18c7aeffab287
luolihaoo <3031191310@qq,com>
2018-05-23 21:01:31 +0800

    第四#4 第四次实验

--- soft1614080902331/app/java/soft1614080902331/saierhaoin.java
+++ soft1614080902331/app/java/soft1614080902331/saierhaoin.java
@@ -1,4 +1,4 @@
-package soft1614080902331.androidlabs.hzuapps.edu.saierhao;
+package edu.hzuapps.androidlabs.soft1614080902331;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -7,19 +7,19 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 
-public class Soft1614080902331 extends AppCompatActivity implements OnClickListener{
+public class saierhaoin extends AppCompatActivity implements OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_saierhaoin);
-        Button button = (Button) findViewById(R.id.button);
+        Button button = (Button) findViewById(R.id.button1);
         button.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
-        Intent intent=new Intent(Soft1614080902331.this,saierhaopk.class);
+        Intent intent=new Intent(saierhaoin.this,saierhaopk.class);
         startActivity(intent);
     }
 
--- soft1614080902331/app/java/soft1614080902331/saierhaozc.java
+++ soft1614080902331/app/java/soft1614080902331/saierhaozc.java
@@ -7,12 +7,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-public class saierhaopk extends AppCompatActivity {
+public class saierhaozc extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_saierhaopk);
+        setContentView(R.layout.activity_saierhaozc);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
