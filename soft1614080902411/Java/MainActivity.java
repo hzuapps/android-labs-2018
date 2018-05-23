027914d4485b83716eecc1a5ae304d4372100cb3
WLIYUN <2531233108@qq.com>
2018-05-23 22:49:12 +0800

    #3 第三次实验

--- soft1614080902411/Java/MainActivity.java
+++ soft1614080902411/Java/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.edu.hzu.myapplication4;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button bt1=(Button)findViewById(R.id.bt1);
+        bt1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,Main2Activity.class);
+                startActivity(intent);
+
+            }
+        });
     }
 }
