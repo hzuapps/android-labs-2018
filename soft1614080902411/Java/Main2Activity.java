76f5b47c52847d81526db32fcf9d1cc1590edf6d
WLIYUN <2531233108@qq.com>
2018-05-24 00:02:59 +0800

    #4 第四次实验

--- soft1614080902411/Java/Main2Activity.java
+++ soft1614080902411/Java/Main2Activity.java
@@ -1,11 +1,13 @@
 package com.example.edu.hzu.myapplication4;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class Main2Activity extends AppCompatActivity {
 
@@ -13,6 +15,14 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        Button bt4=(Button)findViewById(R.id.bt4);
+        bt4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(Main2Activity.this,Main3Activity.class);
+                startActivity(intent);
+            }
+        });
     }
 
 }
