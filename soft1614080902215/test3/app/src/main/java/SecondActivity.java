8a546207e6249bb94f890d3ce56a32831fb3763c
Zhenghizhong <2361976199@qq.com>
2018-05-26 18:10:30 +0800

    #5 #916 #第五次实验+实验报告整理

--- soft1614080902215/test3/app/src/main/java/SecondActivity.java
+++ soft1614080902215/test3/app/src/main/java/SecondActivity.java
@@ -1,14 +1,11 @@
 package androidlabs.hzuapps.edu.soft1614080902215;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.*;
-import android.widget.Button;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -16,7 +13,17 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
     }
 
 }
--- soft1614080902215/test3/app/src/main/java/soft1614080902215MainActivity.java
+++ soft1614080902215/test3/app/src/main/java/soft1614080902215MainActivity.java
@@ -4,24 +4,25 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
+
 import org.w3c.dom.Text;
+
 public class soft1614080902215MainActivity extends AppCompatActivity {
 
-    private Button Button;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_soft1614080902215_main);
 
-        Button=(Button)findViewById(R.id.button);
+        textView=(TextView)findViewById(R.id.textview_01);
 
-        Button.setOnClickListener(new View.OnClickListener() {
+        textView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent(soft1614080902215MainActivity.this,ThirdActivity.class);
+                Intent intent=new Intent(soft1614080902215MainActivity.this,SecondActivity.class);
                 startActivity(intent);
             }
         });
