7d9086507b48b2e52e575fb31f1e7b683327aebb
lixubin51 <2583278069@qq.com>
2018-05-26 15:46:22 +0800

    #5 第五次实验

--- Com1614080901109/app/src/main/java/com/hzu/com1614080901109/TestActivity.java
+++ Com1614080901109/app/src/main/java/com/hzu/com1614080901109/TestActivity.java
@@ -1,4 +1,4 @@
-﻿package edu.androidlabs.com1614080901109;
+package edu.androidlabs.com1614080901109;
 
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
@@ -20,6 +20,30 @@ public class TestActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
     }
-    
+    private void save(String data) {
+        SharedPreferences.Editor editor = getSharedPreferences("data", MODE_PRIVATE).edit();//获取对象，并且命名文件的名称
+        editor.putString("data", data);  //保存数据
+        editor.commit();
+        editor.clear();
+        Toast.makeText(TestActivity.this, "数据已载入", Toast.LENGTH_LONG).show();
+    }
+    private void readPrefs() {
+        SharedPreferences prefs = getSharedPreferences("data", MODE_PRIVATE); //获取对象，读取data文件
+        String hms = prefs.getString("data", ""); //获取文件中的数据
+        Toast.makeText(TestActivity.this, "现存储的数据是："+hms, Toast.LENGTH_LONG).show();
+    }
+
+    public void onClick(View view) {          //按钮的点击事件
+        switch (view.getId()) {
+            case R.id.xieru:
+                EditText ethour = (EditText)findViewById(R.id.hour);
+                data = ethour.getText().toString();
+                save(data);    //调用保存方法，将输入的姓名和年龄保存
+                break;
+            case R.id.duqu:
+                readPrefs();   //调用读取方法，将保存的文件中的姓名和年龄显示出来
+                break;
+        }
+    }
 
 }
