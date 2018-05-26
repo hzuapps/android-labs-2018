21dd1c3a9ad14c0e42ec8b0fcf897717119595f1
hzulinzr <675350941@qq.com>
2018-05-26 22:19:56 +0800

    #6 #911 第六次实验+报告

--- soft1614080902212/app/src/main/java/edu/hzuapps/androidlabs/soft1614080902212/SecondActivity.java
+++ soft1614080902212/app/src/main/java/edu/hzuapps/androidlabs/soft1614080902212/SecondActivity.java
@@ -2,25 +2,33 @@ package edu.hzuapps.androidlabs.soft1614080902212;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-
+import org.json.JSONArray;
+import org.json.JSONObject;
+import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class SecondActivity extends AppCompatActivity {
 
     private Button Button;
     private EditText Edit;
-    private String sr;
+    public String sr;
     private TextView cxjl;
+    private String text;
+    private TextView tv;
+    private String []place=new String[100];
+    private String []time=new String[100];
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,6 +36,7 @@ public class SecondActivity extends AppCompatActivity {
         Button = (Button) findViewById(R.id.button2);
         Edit = (EditText) findViewById(R.id.sr1);
         cxjl = (TextView)findViewById(R.id.cxjl);
+        tv=(TextView)findViewById(R.id.nik);
         Button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -37,13 +46,10 @@ public class SecondActivity extends AppCompatActivity {
                 if (sr.equals("")) {
                     Toast.makeText(SecondActivity.this, "请输入您要查询的时间段", Toast.LENGTH_SHORT).show();
 
-                } else {
-                    Intent intent = new Intent(SecondActivity.this, ThirdActivity.class);
-                    startActivity(intent);
-
                 }
                 String record = cxjl.getText().toString();
                 cxjl.setText(record+"\n"+msg);
+                getJson();
             }
         });
 
@@ -64,22 +70,80 @@ public class SecondActivity extends AppCompatActivity {
             }
             return null;
         }
-
         public void write(String msg){
-
             if (msg == null) return;
             try {
 
                 FileOutputStream fos = openFileOutput("message.txt",
                         MODE_PRIVATE
                 );
-
                 fos.write(msg.getBytes());
-
                 fos.close();
             } catch (Exception e) {
                 e.printStackTrace();
             }
         }
+    public void getJson() {
+        new Thread() {
+            @Override
+            public void run() {
+                try {
+                    //你的URL
+                    String url_s = "https://raw.githubusercontent.com/hzulinzr/android-labs-2018/master/soft1614080902212/S.json";
+                    URL url = new URL(url_s);
+                    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                    conn.setConnectTimeout(5000);
+                    conn.setUseCaches(false);
+                    conn.connect();
+                    InputStream inputStream = conn.getInputStream();
+                    InputStreamReader input = new InputStreamReader(inputStream);
+                    BufferedReader buffer = new BufferedReader(input);
+                    if (conn.getResponseCode() == 200) {
+                        String inputLine;
+                        StringBuffer resultData = new StringBuffer();
+                        while ((inputLine = buffer.readLine()) != null) {
+                            resultData.append(inputLine);
+                        }
+                        text = resultData.toString();
+                        Log.v("out---------------->", text);
+                        parseJson();
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        for(int i=0;i<time.length;i++)
+                        {
+                            if(sr.equals(time[i]))
+                            {
+                                tv.setText(place[i]);
+                                break;
+                            }
+                        }
+                    }
+                });
+            }
+        }.start();
+    }
+
+    public void parseJson() {
+        try {
+            JSONArray jsonArray = new JSONArray(text);
+            for (int i = 0; i < jsonArray.length(); i++) {
+                JSONObject person = jsonArray.getJSONObject(i);
+                 time[i] = person.getString("time");
+                 Log.v("time",time[i]);
+                 place[i]=person.getString("空教室");
+                 Log.v("空教室",place[i]);
+            }
+        } catch (Exception e) {
+            Log.v("出错","Worring");
+            e.printStackTrace();
+        }
+    }
 
 }
+
+
