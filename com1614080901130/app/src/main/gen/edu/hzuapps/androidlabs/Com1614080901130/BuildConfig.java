ba3a7dd1ae868cabc58742a9f6ad5b2b9cbc1382
YoMiao <1055076808@qq.com>
2018-05-27 01:34:17 +0800

    update

--- com1614080901130/app/src/main/gen/edu/hzuapps/androidlabs/Com1614080901130/BuildConfig.java
+++
@@ -1,8 +0,0 @@
-/*___Generated_by_IDEA___*/
-
-package edu.hzuapps.androidlabs.Com1614080901130;
-
-/* This stub is only used by the IDE. It is NOT the BuildConfig class actually packed into the APK */
-public final class BuildConfig {
-  public final static boolean DEBUG = Boolean.parseBoolean(null);
-}
