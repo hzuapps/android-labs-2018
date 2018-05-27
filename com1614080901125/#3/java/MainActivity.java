b7051c122275fd36c1076756a18024e265da0281
Susie-Lau <577221053@qq.com>
2018-05-28 00:51:37 +0800

    #3 #22011 from Susie-Lau

---
+++ com1614080901125/#3/java/MainActivity.java
@@ -0,0 +1,22 @@
+package com1614080901125.androidlabs.hzuapps.edu.hkmacgo;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.view.View;
+
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
+import org.w3c.dom.Text;
+public class MainActivity extends AppCompatActivity {
+            @Override
+     protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+     }
+}
---
+++ com1614080901125/#3/java/SecondActivity.java
@@ -0,0 +1,11 @@
+package com1614080901125.androidlabs.hzuapps.edu.hkmacgo;
+        import android.os.Bundle;
+ import android.support.v7.app.AppCompatActivity;
+ import android.support.v7.widget.Toolbar;
+ import android.view.View;
+         public class SecondActivity extends AppCompatActivity {
+           @Override
+     protected void onCreate(Bundle savedInstanceState) {
+               super.onCreate(savedInstanceState);
+                   }
+ }
