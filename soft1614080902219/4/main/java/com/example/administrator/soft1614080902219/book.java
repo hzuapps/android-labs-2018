2a03c7faf82b89e220810be0e005a92ab9373268
mvp-szf <2444794619@qq.com>
2018-05-27 21:58:05 +0800

    #5 #1286 第5次实验

--- soft1614080902219/4/main/java/com/example/administrator/soft1614080902219/book.java
+++ soft1614080902219/4/main/java/com/example/administrator/soft1614080902219/book.java
@@ -1,14 +1,38 @@
 package com.example.administrator.soft1614080902219;
+import java.io.ByteArrayOutputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 
-import android.os.Bundle;
 import android.app.Activity;
-
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 public class book extends Activity {
-
+    private Button bu;
+    public static final String FILENAME = "setting.set";
+    private Context context=this;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book);
-    }
-
-}
+        bu = (Button) this.findViewById(R.id.classs);
+        bu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String content = bu.getText().toString();
+                try {
+                    // 打开文件获取输出流，文件不存在则自动创建
+                    FileOutputStream fos = openFileOutput(FILENAME,
+                            Context.MODE_PRIVATE);
+                    fos.write(content.getBytes());
+                    fos.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }}
