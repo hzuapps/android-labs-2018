3fc5f5309cd20eccc25b71686a1868e5c7058f38
YoMiao <1055076808@qq.com>
2018-05-27 21:18:38 +0800

    第四次实验

--- com1614080901130/app/src/main/java/edu/hzuapps/androidlabs/Com1614080901130/Share.java
+++ com1614080901130/app/src/main/java/edu/hzuapps/androidlabs/Com1614080901130/Share.java
@@ -1,30 +1,25 @@
 package edu.hzuapps.androidlabs.Com1614080901130;
 
 import android.app.Activity;
-import android.os.Bundle;
-import android.widget.Button;
 import android.content.Intent;
+import android.os.Bundle;
 import android.view.View;
-import android.widget.TextView;
-//import android.widget.TextView;
+import android.widget.Button;
 
 
-public class Anni extends Activity {
 
-//    private TextView textView;
+public class Share extends Activity {
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.anni_home);
-
+        setContentView(R.layout.anni_share);
         final Activity thisActivity = this;
-
-
-        Button btnOpen =  findViewById(R.id.home_button);
+        Button btnOpen =  findViewById(R.id.upload_button);
         btnOpen.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(thisActivity,Show.class);
+                Intent intent = new Intent(thisActivity,Share.class);
                 thisActivity.startActivity(intent);
             }
         });
