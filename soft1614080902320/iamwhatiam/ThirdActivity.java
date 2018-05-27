3b89b71df56a8eeb26991c4c64284f3476cc1763
callmeliqin <140468377@qq.com>
2018-05-27 16:55:03 +0800

    #5 第五次实验

--- soft1614080902320/iamwhatiam/ThirdActivity.java
+++ soft1614080902320/iamwhatiam/ThirdActivity.java
@@ -1,11 +1,24 @@
 package com.example.linliqin.iamwhatiam;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class ThirdActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.third_activity);
+        Button again=(Button)findViewById(R.id.go02);
+        again.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(ThirdActivity.this,FileStorageActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
+
 }
