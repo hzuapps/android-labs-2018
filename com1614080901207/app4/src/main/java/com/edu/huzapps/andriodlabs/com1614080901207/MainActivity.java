273b66de297e6939f160fdca483d0b083c021e4a
BlueDuper <1442966070@qq.com>
2018-05-27 10:09:16 +0800

    #6 第六次实验

---
+++ com1614080901207/app4/src/main/java/com/edu/huzapps/andriodlabs/com1614080901207/MainActivity.java
@@ -0,0 +1,25 @@
+package com.edu.huzapps.andriodlabs.com1614080901207;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    protected Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,Main3Activity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
