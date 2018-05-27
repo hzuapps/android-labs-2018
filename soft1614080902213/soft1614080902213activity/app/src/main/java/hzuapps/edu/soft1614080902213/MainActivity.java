f07c5d7af1205dc5750a3e4955d1301ebacd74c9
HZUHJ <37758580+HZUHJ@users.noreply.github.com>
2018-05-27 14:01:07 +0800

    #6 #2096 update 第六次实验内容

--- soft1614080902213/soft1614080902213activity/app/src/main/java/hzuapps/edu/soft1614080902213/MainActivity.java
+++ soft1614080902213/soft1614080902213activity/app/src/main/java/hzuapps/edu/soft1614080902213/MainActivity.java
@@ -93,8 +93,9 @@ public class MainActivity extends AppCompatActivity
         } else if (id == R.id.nav_share) {
 
         } else if (id == R.id.nav_send) {
-
+            startActivity(new Intent(MainActivity.this,Main2Activity.class));
         }
+
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
