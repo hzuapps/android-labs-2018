cb5a48194186afcc9b05062b11bbf8b49c4db1c4
unknown <>
2018-05-28 01:20:16 +0800

    #4第四次实验

--- com1614080901102/app/src/main/java/androidlabs/hzuapps/edu/com1614080901102/SecordActivity.java
+++ com1614080901102/app/src/main/java/androidlabs/hzuapps/edu/com1614080901102/SecordActivity.java
@@ -1,16 +1,28 @@
 package androidlabs.hzuapps.edu.com1614080901102;
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.os.Bundle;
 import android.view.View;
-public class SecordActivity {
+import android.widget.LinearLayout;
+public class SecondActivity extends AppCompatActivity {
+
+    private LinearLayout s_eyu;
+    private LinearLayout s_ruiwen;
+    private LinearLayout s_yasuo;
+    private LinearLayout s_huangzi;
+    private LinearLayout s_timo;
+    private LinearLayout s_nuoshou;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_com1614080901102);
 
-   }
-}
+        s_eyu=(LinearLayout)findViewById(R.id.ll_eyu);
+        s_ruiwen=(LinearLayout)findViewById(R.id.ll_ruiwen);
+        s_yasuo=(LinearLayout)findViewById(R.id.ll_yasuo);
+        s_huangzi=(LinearLayout)findViewById(R.id.ll_huangzi);
+        s_timo=(LinearLayout)findViewById(R.id.ll_timo);
+        s_nuoshou=(LinearLayout)findViewById(R.id.ll_nuoshou);
+
