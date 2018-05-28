4d4c1ea0227ebbc559ad5e121ceadae8e360457a
mvp-szf <315189685@qq.com>
2018-05-28 20:57:39 +0800

    #6 #963 第六次实验

---
+++ soft1614080902238/xg/app/src/main/java/edu/hzuapps/androidlabs/MainActivity.java
@@ -0,0 +1,43 @@
+package edu.hzuapps.androidlabs;
+
+
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+
+
+    Button finish, startActivity;
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final Activity thisActivity = this;
+        finish = (Button) findViewById(R.id.finish);
+        startActivity = (Button) findViewById(R.id.startActivity);
+        startActivity.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View source) {
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                thisActivity.startActivity(intent);
+
+            }
+        });
+     finish = (Button) findViewById(R.id.finish);
+        finish.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View source) {
+                Intent intent = new Intent(MainActivity.this, ThreeActivity.class);
+                thisActivity.startActivity(intent);
+
+            }
+        });
+
+
+    }
+}
