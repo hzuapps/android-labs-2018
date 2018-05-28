36af1513fee62e242988372b0ef7732893a49b5e
Rvelamen <13126348842@163.com>
2018-05-28 21:41:09 +0800

    #5#937#第五次实验

--- soft1614080902130/Soft1614080902130Activity.java
+++
@@ -1,8 +0,0 @@
-package edu.hzuapps.androidlabs.soft1614080902130;
-
-public class Sotf1614080902130Activity {
- 
-public static void main(String args[]){
-    System.out.println("Hello World!!!");
-} 
-}
