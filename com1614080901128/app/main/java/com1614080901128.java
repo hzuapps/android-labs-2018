e2884b6b9088cc7d4e5b51dc08f493a0679ef137
lizhenyong98 <724605552@qq.com>
2018-05-28 12:54:18 +0800

    #2实验二

---
+++ com1614080901128/app/main/java/com1614080901128.java
@@ -0,0 +1,13 @@
+package com.example.com1614080901128.com1614080901128activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class com1614080901128 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_com1614080901128);
+    }
+}
