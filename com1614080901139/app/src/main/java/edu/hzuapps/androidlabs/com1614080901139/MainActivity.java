ccc07d79902dea4d7fd31cb035247391e26b03c0
spemed <37761110+spemed@users.noreply.github.com>
2018-05-28 20:09:06 +0800

    #5 #1528 第五次实验

--- com1614080901139/app/src/main/java/edu/hzuapps/androidlabs/com1614080901139/MainActivity.java
+++ com1614080901139/app/src/main/java/edu/hzuapps/androidlabs/com1614080901139/MainActivity.java
@@ -1,10 +1,14 @@
 package edu.hzuapps.androidlabs.com1614080901139;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,5 +29,20 @@ public class MainActivity extends AppCompatActivity {
         et_passWord=(EditText)findViewById(R.id.et_passWord);
         tv_forgetPassWord=(TextView)findViewById(R.id.tv_forgetPassWord);
         bt_login=(Button)findViewById(R.id.bt_login);
+        bt_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String userName=et_userName.getText().toString();
+                String passWord=et_passWord.getText().toString();
+                SharedPreferences.Editor editor=getSharedPreferences("user",MODE_PRIVATE).edit();
+                editor.putString("userName",userName);
+                editor.putString("passWord",passWord);
+                editor.apply();
+                Toast.makeText(MainActivity.this,"登录成功",Toast.LENGTH_LONG).show();
+
+                Intent intent=new Intent(MainActivity.this,Main2Activity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
