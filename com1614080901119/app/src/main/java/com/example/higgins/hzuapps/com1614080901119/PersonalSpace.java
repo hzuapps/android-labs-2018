3d057e2c2627c6d6a5e6dcbadf01a4a78566f98f
h-higgins <2352590909@qq.com>
2018-05-28 14:04:20 +0800

    #4 #1333 第四次实验

--- com1614080901119/app/src/main/java/com/example/higgins/hzuapps/com1614080901119/PersonalSpace.java
+++ com1614080901119/app/src/main/java/com/example/higgins/hzuapps/com1614080901119/PersonalSpace.java
@@ -1,15 +1,36 @@
 package com.example.higgins.hzuapps.com1614080901119;
 
-import android.os.Bundle;
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
-
-import static com.example.higgins.hzuapps.com1614080901119.R.*;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutCompat;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class PersonalSpace extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(layout.space_personal);
+        setContentView(R.layout.space_personal);
+
+        final Activity thisActivity = this;
+
+        Button btn = (Button)findViewById(R.id.summitevents);
+        btn.setOnClickListener(new View.OnClickListener(){
+
+            public void onClick(View v){
+
+                LinearLayoutCompat ll = (LinearLayoutCompat)findViewById(R.id.myLayout);
+                TextView mTextView = new TextView(thisActivity);
+                mTextView.setText("做课程设计");
+                ll.addView(mTextView);
+
+
+            }
+        });
     }
 }
