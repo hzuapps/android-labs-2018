2e98d3e6d701459a45b60f2a626c0b7b2409d50d
WUJINH <776052998@qq,com>
2018-05-29 00:07:14 +0800

    #3#1307第三次实验

--- soft1614080902126/app/src/main/java/edu/hzuapps/androidlabs/soft1614080902126/Soft1614080902126Activity.java
+++ soft1614080902126/app/src/main/java/edu/hzuapps/androidlabs/soft1614080902126/Soft1614080902126Activity.java
@@ -1,5 +1,32 @@
 package edu.hzuapps.androidlabs.soft1614080902126;
 
-public class Soft1614080902126Activity{
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class Soft1614080902126Activity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_soft1614080902126);
+        Button button1=(Button)findViewById(R.id.button_1);
+        button1.setOnClickListener(new View.OnClickListener(){
+
+            public void onClick(View v){
+                Intent intent=new Intent(Soft1614080902126Activity.this,SecondActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+    }
+
+
 
 }
