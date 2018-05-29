049665e9c518ec684c0dddfd5bf28dda157ed789
lhyyhl <3132757812@qq.com>
2018-05-30 00:36:49 +0800

    #6 #meitu

---
+++ soft1614080902304/main/java/edu/hzuapps/androidlabs/soft1614080902304/soft1614080902304/Check.java
@@ -0,0 +1,85 @@
+package edu.hzuapps.androidlabs.soft1614080902304.soft1614080902304;
+
+/**
+ * Created by 李惠怡 on 2018/5/29.
+ */
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.os.Environment;
+import android.os.Handler;
+import android.os.Message;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class Check extends Activity {
+    private EditText editText;
+    private Button button;
+    private ImageView imageView;
+    private Bitmap bitmap;
+    //手柄更新的作用
+    Handler handler=new Handler(){
+        public void handleMessage(Message msg) {
+            if(msg.what==111){
+                imageView.setImageBitmap(bitmap);
+            }
+        };
+    };
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.check);
+        //初始化组件
+        editText=(EditText) findViewById(R.id.view2);
+        button=(Button) findViewById(R.id.btn_3);
+        imageView=(ImageView) findViewById(R.id.imview);
+        //给下载按钮添加一个监听
+        button.setOnClickListener(new OnClickListener() {
+            public void onClick(View arg0) {
+                new Thread(t).start();
+            }
+        });
+    }
+    //为了下载图片资源，开辟一个新的子线程
+    Thread t=new Thread(){
+        public void run() {
+            //下载图片的路径
+            String iPath=editText.getText().toString();
+            try {
+                //对资源链接
+                URL url=new URL(iPath);
+                //打开输入流
+                InputStream inputStream=url.openStream();
+                //对网上资源进行下载转换位图图片
+                bitmap=BitmapFactory.decodeStream(inputStream);
+                handler.sendEmptyMessage(111);
+                inputStream.close();
+                //再一次打开
+                inputStream=url.openStream();
+                File file=new File(Environment.getExternalStorageDirectory()+"/DCIM/");
+                FileOutputStream fileOutputStream=new FileOutputStream(file);
+                int hasRead=0;
+                while((hasRead=inputStream.read())!=-1){
+                    fileOutputStream.write(hasRead);
+                }
+                fileOutputStream.close();
+                inputStream.close();
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        };
+    };
+}
