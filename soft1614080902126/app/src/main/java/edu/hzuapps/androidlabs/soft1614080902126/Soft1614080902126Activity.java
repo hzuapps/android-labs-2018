1e7513e3dcd667828347fa1d7869cb9ad178f0ce
WUJINH <776052998&qq.com>
2018-05-29 12:47:14 +0800

    #4#1307第四次实验

--- soft1614080902126/app/src/main/java/edu/hzuapps/androidlabs/soft1614080902126/Soft1614080902126Activity.java
+++ soft1614080902126/app/src/main/java/edu/hzuapps/androidlabs/soft1614080902126/Soft1614080902126Activity.java
@@ -1,12 +1,10 @@
 package edu.hzuapps.androidlabs.soft1614080902126;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 
 public class Soft1614080902126Activity extends AppCompatActivity{
 
