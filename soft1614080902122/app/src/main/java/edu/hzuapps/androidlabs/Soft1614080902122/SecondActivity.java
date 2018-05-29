c3a8f5f625dfaea8874a3faa94748db90cdad090
chenzhiH <37758546+chenzhiH@users.noreply.github.com>
2018-05-29 21:52:36 +0800

    Add files via upload

--- soft1614080902122/app/src/main/java/edu/hzuapps/androidlabs/Soft1614080902122/SecondActivity.java
+++ soft1614080902122/app/src/main/java/edu/hzuapps/androidlabs/Soft1614080902122/SecondActivity.java
@@ -1,13 +1,16 @@
 package edu.hzuapps.androidlabs.Soft1614080902122;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +26,23 @@ public class SecondActivity extends AppCompatActivity {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
+        });
+        textView=(TextView)findViewById(R.id.textview_02);
+
+
+
+        textView.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+
+            public void onClick(View v) {
+
+                Intent intent=new Intent(SecondActivity.this,ThreeActivity.class);
+
+                startActivity(intent);
+
+            }
+
         });
     }
 
