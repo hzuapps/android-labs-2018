49cfad3b6fff88c3a88130c1f2ce6d0f3719f8df
lili503946540 <503946540@qq.com>
2018-05-29 21:58:40 +0800

    #第4次实验

--- soft1606070302104/app/src/main/java/edu/hzuapps/androidlabs2018/soft1606070302104/Soft1606070302104Activity.java
+++ soft1606070302104/app/src/main/java/edu/hzuapps/androidlabs2018/soft1606070302104/Soft1606070302104Activity.java
@@ -9,12 +9,15 @@ import android.widget.Button;
 
 public class Soft1606070302104Activity extends AppCompatActivity {
     private Button btn;
+    private Button btn0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_soft1606070302104);
         btn = (Button) findViewById(R.id.button);
         btn.setOnClickListener(new MyButtonListener() );
+        btn0 = (Button) findViewById(R.id.button0);
+        btn0.setOnClickListener(new MyButtonListener0() );
     }
     //这个属于一个内部类
     class MyButtonListener implements OnClickListener {
@@ -25,10 +28,21 @@ public class Soft1606070302104Activity extends AppCompatActivity {
             Intent intent = new Intent();
             //设置从哪个activity启动哪个activity
             intent.setClass(Soft1606070302104Activity.this, second2Activity.class);
-            //把一个值写入到Intent中
-            //intent.putExtra("Text", "测试值");
             //启动另一个activity
             Soft1606070302104Activity.this.startActivity(intent);
+        }
+    }
+
+    class MyButtonListener0 implements OnClickListener {
+        //
+        public void onClick(View v) {
+            // TODO Auto-generated method stub
+            //创建一个Intent的实例
+            Intent intent0 = new Intent();
+            //设置从哪个activity启动哪个activity
+            intent0.setClass(Soft1606070302104Activity.this, third3Activity.class);
+            //启动另一个activity
+            Soft1606070302104Activity.this.startActivity(intent0);
         }
     }
 }
