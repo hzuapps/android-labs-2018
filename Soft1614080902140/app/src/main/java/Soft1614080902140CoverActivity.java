5fdd043a50dc206e5861c33be0b4c70827c80ee1
czwly <1095948152@qq.com>
2018-05-30 18:15:31 +0800

    #第四次实验

--- Soft1614080902140/app/src/main/java/Soft1614080902140CoverActivity.java
+++ Soft1614080902140/app/src/main/java/Soft1614080902140CoverActivity.java
@@ -5,19 +5,20 @@ import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 public class Soft1614080902140CoverActivity extends AppCompatActivity {
 
-    private ImageView imageView;
+    private Button button5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_soft1614080902140_cover);
 
-        imageView=(ImageView) findViewById(R.id.image_view);
-        imageView.setOnClickListener(new View.OnClickListener() {
+        button5=(Button) findViewById(R.id.button5);
+        button5.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent=new Intent(Soft1614080902140CoverActivity.this,Soft1614080902140HomeActivity.class);
