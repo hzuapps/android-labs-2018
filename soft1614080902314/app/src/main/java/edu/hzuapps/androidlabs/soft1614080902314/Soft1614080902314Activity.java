0660e037e599b920b8bddca23ac6c317e001b1b6
Zhengmianjie <1254007862@qq.com>
2018-05-30 22:26:01 +0800

    #6 #978 实验六

--- soft1614080902314/app/src/main/java/edu/hzuapps/androidlabs/soft1614080902314/Soft1614080902314Activity.java
+++ soft1614080902314/app/src/main/java/edu/hzuapps/androidlabs/soft1614080902314/Soft1614080902314Activity.java
@@ -1,8 +1,8 @@
 package edu.hzuapps.androidlabs.soft1614080902314;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -34,8 +34,18 @@ public class Soft1614080902314Activity extends AppCompatActivity {
             public void onClick(View v) {
                 S=(EditText) findViewById(R.id.editText3);
                 saveContent();
+                    Intent intent = new Intent();
+                    intent.setClass(Soft1614080902314Activity.this,soft1614080902314Activity3.class);
+                    startActivity(intent);
+
+            }
+        });
+        Button button3 = (Button)findViewById(R.id.button4);
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
                 Intent intent = new Intent();
-                intent.setClass(Soft1614080902314Activity.this,soft1614080902314Activity3.class);
+                intent.setClass(Soft1614080902314Activity.this,Soft1614080902314Activity4.class);
                 startActivity(intent);
             }
         });
