976b2287a69a21ad4013f62c407d178e6631e70e
13435107864 <810488315@qq.com>
2018-05-30 19:40:06 +0800

    #2 #1960 第二次实验修改

--- soft1614080902107/Soft1614080902107MainActivity.java
+++ soft1614080902107/Soft1614080902107MainActivity.java
@@ -3,7 +3,7 @@ package com.example.jerry.soft1614080902107;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Soft1614080902107MainActivity extends AppCompatActivity {
+public class soft1614080902107MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
