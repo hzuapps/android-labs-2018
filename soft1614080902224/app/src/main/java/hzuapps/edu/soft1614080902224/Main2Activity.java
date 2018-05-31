5778a27dd19099d28cf772b799cffa8f43c9036c
woshilinglei <504192390@qq.com>
2018-05-31 23:48:37 +0800

    #4 第四次实验

--- soft1614080902224/app/src/main/java/hzuapps/edu/soft1614080902224/Main2Activity.java
+++ soft1614080902224/app/src/main/java/hzuapps/edu/soft1614080902224/Main2Activity.java
@@ -1,7 +1,10 @@
 package com.example.soft1614080902224;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Main2Activity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        Button button=findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(Main2Activity.this,Main3Activity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
