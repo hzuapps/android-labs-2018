1fff9dab48ec43547b6e291416bc8446a7888d24
uzi-up <a1111z5@qq.com>
2018-05-31 21:35:59 +0800

    #5 第五次实验

---
+++ soft1614080902401/实验5/java/com/example/qsi/soft1614080902401activity/Soft1614080902401.java
@@ -0,0 +1,139 @@
+package com.example.qsi.soft1614080902401activity;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FilenameFilter;
+import java.io.IOException;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.Toast;
+import java.io.File;
+import java.lang.String;
+
+public class Soft1614080902401 extends AppCompatActivity {
+    public static final String DIRECTORY = "demo";
+    public static final String FILENAME = "file_demo.txt";
+    public static final String TAG = Soft16140809024012.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_soft1614080902401);
+        final Soft1614080902401 thisActivity = this;
+    }
+
+
+    public void lishijilu(View view) {
+        //点击按钮触发提示信息
+        Button btn = (Button)findViewById(R.id.button);
+        Toast.makeText(this,"查询成功",Toast.LENGTH_SHORT).show();
+
+        String text = ((EditText) findViewById(R.id.editText)).getText().toString();
+        saveTextIntoInternalStorage(text);
+    }
+
+    public void jilu(View view) {
+        //实现点击该按钮后跳转到指定界面
+        Intent intent = new Intent(Soft1614080902401.this, Soft16140809024012.class);
+        startActivity(intent);
+    }
+    // 将文字保存到内部存储
+    private void saveTextIntoInternalStorage(String text) {
+        // 获取内部存储目录
+        File dir = this.getFilesDir();
+        //File dir = getCacheDir();
+
+        if (dir.isDirectory()) {
+//            dir.mkdir();
+//            dir.mkdirs();
+        }
+
+        if (dir.isFile()) {
+            // D:/Abc.txt , -> D:/Abc1.txt ->D:/abc.txt
+        }
+
+        File file = new File(dir, FILENAME);
+
+//        try {
+//            File = File.createTempFile(FILENAME, null, dir);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+
+        if (file.exists()) { // 判断文件是否存在
+            Log.i(TAG, file.getAbsolutePath());
+            Log.i(TAG, file.length() + ""); // bytes*1024=kb *1024 MB
+            Log.i(TAG, file.isFile() + "");
+            file.canRead();
+            file.canWrite();
+            file.canExecute();
+
+            file.getFreeSpace();
+            file.getTotalSpace();
+        }
+
+        FileOutputStream fos = null;  // 字节流  | char | cn : gbk 2 bytes, utf8 3 bytes
+
+        try { // 使用API打开输出流
+            fos = openFileOutput(FILENAME, MODE_PRIVATE);
+            //FileOutputStream fos = new FileOutputStream(file);
+            fos.write(text.getBytes()); // 写入内容
+            fos.close(); // 关闭流
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                fos.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        FileReader reader = null; // char
+
+        try {
+            reader = new FileReader(file.getAbsoluteFile());
+            BufferedReader bReader = new BufferedReader(reader);
+            String line = bReader.readLine();
+            Log.i(TAG, "从文件读取的内容: " + line);
+            bReader.close();
+            reader.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // 显示结果
+        showResult(file.getAbsolutePath());
+
+        // 删除文件
+        // file.delete();
+        // deleteFile(FILENAME);
+    }
+    private void showResult(String result) {
+        ((TextView) findViewById(R.id.editText2)) //
+                .setText(result.toCharArray(), 0, result.length());
+    }
+
+
+}
