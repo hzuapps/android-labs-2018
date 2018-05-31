9f9172c5df3ba0585e020fc078059fdb4d882c7f
ZZZZmin <1310720114@qq.com>
2018-05-31 23:33:33 +0800

    “#5

--- soft1613071002206/app/src/main/java/edu/hzuapps/androidlabs2018/soft1613071002206/MainActivity.java
+++ soft1613071002206/app/src/main/java/edu/hzuapps/androidlabs2018/soft1613071002206/MainActivity.java
@@ -3,17 +3,28 @@ package edu.hzuapps.androidlabs2018.soft1613071002206;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.Intent;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
-
-    private String Value;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Intent myIntent = getIntent();
+        Button btn1=(Button)findViewById(R.id.btn1);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,Main3Activity.class);
+                startActivity(intent);
+            }
+        });
 
-        String TextValue = myIntent.getStringExtra("Text");
+
+
     }
+
 }
