3647f72cb812d2a2fbfbdc6db2e151bbd098fa75
lazytea <2247952593@qq.com>
2018-05-31 23:14:08 +0800

    ““实验四”

--- soft1614080902311/app/src/main/java/edu/hzu/take_out/MainActivity.java
+++ soft1614080902311/app/src/main/java/edu/hzu/take_out/MainActivity.java
@@ -5,7 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,25 +15,43 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button btn_jmp = findViewById(R.id.btn_jmp);
+        ImageButton ib_add=findViewById(R.id.ib_add);
+
+
         btn_jmp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(MainActivity.this, "美味与你常在", Toast.LENGTH_SHORT).show();
                 Intent it=new Intent();
-                it.setClass(MainActivity.this,SecondActivity.class);
+                it.setClass(MainActivity.this,Activity3.class);
+                startActivity(it);
+            }
+        });
+        ib_add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent it=new Intent();
+                it.setClass(MainActivity.this,Activity2.class);
                 startActivity(it);
             }
         });
 
     }
 
-    public class SecondActivity extends AppCompatActivity {
+    public class Activity2 extends AppCompatActivity {
         @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity2);
         }
     }
+
+    public class Activity3 extends AppCompatActivity {
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity3);
+        }
+    }
+
 }
-
-
