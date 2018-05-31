838a89c179ccde9ab36f27305ffa5f7029113e2a
AisakaTaika <37798420+AisakaTaika@users.noreply.github.com>
2018-05-31 23:16:44 +0800

    #3 第三次实验

---
+++ soft1614080902435/app/src/main/java/edu/hzuapps/androidlabs/Soft1614080902435/jiemianerActivity.java
@@ -0,0 +1,14 @@
+package edu.hzuapps.androidlabs.Soft1614080902435;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class jiemianerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView( R.layout.activity_jiemianer);
+    }
+
+}
