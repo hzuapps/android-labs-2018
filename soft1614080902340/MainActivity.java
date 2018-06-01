f2213cbe53b0f1c76e27f2b1cbcfb7b538803bb9
chenzxer <2544717340@qq.com>
2018-06-01 22:58:43 +0800

    #1011 shiyansi

--- soft1614080902340/MainActivity.java
+++ soft1614080902340/MainActivity.java
@@ -1,11 +1,7 @@
-package com.example.asus.myapplication;
+package com.example.asus;
 
-import android.app.Activity;
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,14 +9,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-         final Activity MainActivity = this;
-        Button bt =(Button) findViewById(R.id.button);
-        bt.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity,SecondActivity.class);
-                MainActivity.startActivity(intent);
-            }
-        });
     }
 }
