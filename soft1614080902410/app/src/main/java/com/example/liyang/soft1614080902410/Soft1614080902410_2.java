38c47bdb4dfa45b38fb224ef5badb278c948db46
liyanghey <1501077693@qq.com>
2018-06-02 02:55:56 +0800

    #5 #10311第五次实验

--- soft1614080902410/app/src/main/java/com/example/liyang/soft1614080902410/Soft1614080902410_2.java
+++ soft1614080902410/app/src/main/java/com/example/liyang/soft1614080902410/Soft1614080902410_2.java
@@ -2,12 +2,25 @@ package com.example.liyang.soft1614080902410;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class Soft1614080902410_2 extends AppCompatActivity {
+    private TextView textView;
+    public static final String DIRECTORY = "demo";
+    public static final String FILENAME = "file_demo.txt";
+
+    public static final String TAG = Soft1614080902410_2.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,5 +44,74 @@ public class Soft1614080902410_2 extends AppCompatActivity {
                 thisActivity.startActivity(intent2);
             }
         });
+
+        String text = "content";
+        saveTextIntoExternalStorage(text);
+    }
+
+    private void saveTextIntoExternalStorage(String text) {
+        if (!isExternalStorageWritable()) {
+            Log.e(TAG, "外部存储不可写!");
+            return;
+        }
+
+        File dir = getPublicExtStorageDir(DIRECTORY, Environment.DIRECTORY_DOWNLOADS);
+        File file = new File(dir, FILENAME);
+
+        try {
+            FileOutputStream fos = new FileOutputStream(file);
+            fos.write(text.getBytes());
+            fos.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // 显示结果
+        showResult(file.getAbsolutePath());
+    }
+
+    /* Checks if external storage is available for read and write */
+    private boolean isExternalStorageWritable() {
+        String state = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(state)) {
+            return true;
+        }
+        return false;
+    }
+
+    /* Checks if external storage is available to at least read */
+    private boolean isExternalStorageReadable() {
+        String state = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(state) ||
+                Environment.MEDIA_MOUNTED_READ_ONLY.equals(state)) {
+            return true;
+        }
+        return false;
+    }
+
+    // 创建公开的外部存储目录（App卸载时不会删除）
+    private File getPublicExtStorageDir(String dirName, String type) {
+        if (type == null) { // 指定文件类型
+            type = Environment.DIRECTORY_PICTURES;
+        }
+        File dir = new File(Environment //
+                .getExternalStoragePublicDirectory(type), dirName);
+        if (!dir.mkdirs()) {
+            Log.e(TAG, "目录无法创建!");
+        }
+
+        long freeSpace = dir.getFreeSpace();
+        Log.i(TAG, "剩余空间大小: " + (freeSpace / 1024 / 1024) + "MB");
+        long totalSpace = dir.getTotalSpace();
+        Log.i(TAG, "总空间大小: " + (totalSpace / 1024 / 1024) + "MB");
+
+        return dir;
+    }
+
+    private void showResult(String result) {
+        ((TextView) findViewById(R.id.text_path))//
+                .setText(result.toCharArray(), 0, result.length());
     }
 }
