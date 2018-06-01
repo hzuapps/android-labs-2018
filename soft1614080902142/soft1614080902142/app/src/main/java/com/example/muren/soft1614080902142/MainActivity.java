4729dedba4700a084cd9c3bef1d8dc1076c79519
HandsomeFace <737377809@qq.com>
2018-06-01 19:19:24 +0800

    #3#1614080902142第三次试验

---
+++ soft1614080902142/soft1614080902142/app/src/main/java/com/example/muren/soft1614080902142/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.muren.soft1614080902142;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
