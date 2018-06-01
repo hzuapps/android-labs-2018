a1ef1325b115f5c67ab6d1c50904a0f43e707bb8
li763407418 <763407418@qq.com>
2018-06-01 21:34:12 +0800

    #6 #1185

--- soft1614080902325/java/edu/hzuapps/androidlabs/personalInformationActivity.java
+++ soft1614080902325/java/edu/hzuapps/androidlabs/personalInformationActivity.java
@@ -8,12 +8,13 @@ import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.target.ImageViewTargetFactory;
 
 public class personalInformationActivity extends AppCompatActivity {
-
+    private ImageView tv_11;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_personal_information);
-
+        tv_11=findViewById(R.id.tv_11);
+        Glide.with(this).load("http://p8.qhmsg.com/dr/217_214_/t01fe2c1732a6a9a83d.jpg").into((ImageView) tv_11);
     }
 
 
