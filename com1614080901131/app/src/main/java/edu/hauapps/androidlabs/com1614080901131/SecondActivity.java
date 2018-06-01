7e6fc54c6adb55e2e26847d3b93b3a6c51a25c94
pa-kou <313261906@qq.com>
2018-06-01 20:51:37 +0800

    #6 实验6

--- com1614080901131/app/src/main/java/edu/hauapps/androidlabs/com1614080901131/SecondActivity.java
+++ com1614080901131/app/src/main/java/edu/hauapps/androidlabs/com1614080901131/SecondActivity.java
@@ -4,14 +4,34 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.ListView;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class SecondActivity extends AppCompatActivity {
     private Button bt_get;
     private TextView tv_text;
+    private String text;
+    private ListView listView;
+    private ArrayAdapter<String> adapter;
+    private List<String> list=new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,5 +51,68 @@ public class SecondActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        listView=(ListView)findViewById(R.id.listV);
+        getjson();
+    }
+
+    public void getjson()
+    {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    URL url = new URL("https:/raw.githubusercontent.com/JianPengChen/android-labs-2018/master/Com1614080901111/test.json");
+                    HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                    connection.setRequestMethod("GET");
+                    connection.setConnectTimeout(8000);
+                    connection.setReadTimeout(8000);
+                    connection.setUseCaches(false);
+                    connection.connect();
+                    InputStream in = connection.getInputStream();
+                    InputStreamReader input=new InputStreamReader(in);
+                    BufferedReader reader=new BufferedReader(input);
+                    if (connection.getResponseCode() == 200) {
+                        StringBuilder response = new StringBuilder();
+                        String Line;
+                        while ((Line = reader.readLine()) != null) {
+                            response.append(Line);
+                        }
+                        text = response.toString();
+                        Log.v("out---------------->",text);
+                        analyzejson();
+                    }
+                }
+                catch(Exception e)
+                {
+                    e.printStackTrace();
+                }
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        adapter=new ArrayAdapter<String>(SecondActivity.this,android.R.layout.simple_list_item_1,list);
+                        listView.setAdapter(adapter);
+                    }
+                });
+            }
+        }).start();
+    }
+
+    public void analyzejson()
+    {
+        try{
+            JSONArray jsonArray=new JSONArray(text);
+            for(int a=0;a<jsonArray.length();a++)
+            {
+                JSONObject jsonObject=jsonArray.getJSONObject(a);
+                String content=jsonObject.getString("con");
+                list.add(content);
+                Log.v("结果",content);
+            }
+
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
     }
 }
