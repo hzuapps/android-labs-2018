de74f3aa6a79ca7462afd3dd8c20600f93fb6ce2
li763407418 <763407418@qq.com>
2018-06-01 14:28:13 +0800

    Signed-off-by: li763407418 <763407418@qq.com>

--- soft1614080902325/java/edu/hzuapps/androidlabs/zuozheActivity.java
+++ soft1614080902325/java/edu/hzuapps/androidlabs/zuozheActivity.java
@@ -2,19 +2,17 @@ package edu.hzuapps.androidlabs.soft1614080902325;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
-import com.bumptech.glide.request.target.ImageViewTargetFactory;
 
-public class personalInformationActivity extends AppCompatActivity {
+public class zuozheActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_personal_information);
+        setContentView(R.layout.activity_zuozhe);
     }
-
-
-
 }
