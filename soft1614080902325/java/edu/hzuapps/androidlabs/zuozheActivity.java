c53ce3a62be70479c0fe5c508d703063798fd4fa
li763407418 <763407418@qq.com>
2018-06-01 19:56:43 +0800

    #6 #1185“ git commit -m #6

--- soft1614080902325/java/edu/hzuapps/androidlabs/zuozheActivity.java
+++ soft1614080902325/java/edu/hzuapps/androidlabs/zuozheActivity.java
@@ -14,5 +14,6 @@ public class zuozheActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_zuozhe);
+
     }
 }
