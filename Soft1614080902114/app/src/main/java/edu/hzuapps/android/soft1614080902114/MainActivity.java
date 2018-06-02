995119cf7b807c9bcd432cdae5557ab3aff203b2
aLesion <717876241@qq.com>
2018-06-02 12:27:11 +0800

    #4 惠大考试复习平台

--- Soft1614080902114/app/src/main/java/edu/hzuapps/android/soft1614080902114/MainActivity.java
+++ Soft1614080902114/app/src/main/java/edu/hzuapps/android/soft1614080902114/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.soft1614080902114;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button1=findViewById(R.id.button1);
+        Button button2=findViewById(R.id.button2);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,Main2Activity.class);
+                startActivity(intent);
+            }
+        });
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+        public void onClick(View v) {
+            Intent intent=new Intent(MainActivity.this,Main3Activity.class);
+            startActivity(intent);
+        }
+    });
     }
 }
