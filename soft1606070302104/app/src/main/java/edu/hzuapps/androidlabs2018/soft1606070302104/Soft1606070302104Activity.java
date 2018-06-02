e79c1a38567f611d9b2f4a064e395a75a590198f
lili503946540 <503946540@qq.com>
2018-06-02 23:53:39 +0800

    #第六次实验

--- soft1606070302104/app/src/main/java/edu/hzuapps/androidlabs2018/soft1606070302104/Soft1606070302104Activity.java
+++ soft1606070302104/app/src/main/java/edu/hzuapps/androidlabs2018/soft1606070302104/Soft1606070302104Activity.java
@@ -11,6 +11,7 @@ public class Soft1606070302104Activity extends AppCompatActivity {
     private Button btn;
     private Button btn0;
     private Button btn1;
+    private Button btn2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +22,8 @@ public class Soft1606070302104Activity extends AppCompatActivity {
         btn0.setOnClickListener(new MyButtonListenera() );
         btn1 = (Button) findViewById(R.id.button1);
         btn1.setOnClickListener(new MyButtonListenerb() );
+        btn2 = (Button) findViewById(R.id.button2);
+        btn2.setOnClickListener(new MyButtonListenerc() );
     }
     //这个属于一个内部类
     class MyButtonListener implements OnClickListener {
@@ -63,6 +66,21 @@ public class Soft1606070302104Activity extends AppCompatActivity {
             //设置从哪个activity启动哪个activity
 
             intent.setClass(Soft1606070302104Activity.this, FourthActivity.class);
+            //启动另一个activity
+
+            Soft1606070302104Activity.this.startActivity(intent);
+        }
+    }
+    class MyButtonListenerc implements OnClickListener {
+        //
+        public void onClick(View v) {
+            // TODO Auto-generated method stub
+            //创建一个Intent的实例
+
+            Intent intent= new Intent();
+            //设置从哪个activity启动哪个activity
+
+            intent.setClass(Soft1606070302104Activity.this, FifthActivity.class);
             //启动另一个activity
 
             Soft1606070302104Activity.this.startActivity(intent);
