bb67df074e65fda1ce36ada443f6b741f9aaacc4
王卫民 <1597935114@qq.com>
2018-06-02 12:49:14 +0800

    #5 第五次实验报告

--- soft1614080902434/2.app/2.app/src/main/java/edu/hzupps/androidlabs/soft1614080902434/Second_Activity.java
+++ soft1614080902434/2.app/2.app/src/main/java/edu/hzupps/androidlabs/soft1614080902434/Second_Activity.java
@@ -1,44 +1,76 @@
 package soft1614080902434.androidlabs.hzuapps.edu.soft1614080902434;
 
+import android.content.Context;
+import android.support.annotation.RequiresPermission;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
 public class Second_Activity extends AppCompatActivity implements View.OnClickListener {
-   TextView t[] = new TextView[12];
+   EditText ed;
+   Button send;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second_);
+        this.RegisterEcent();
     }
     public void RegisterEcent(){
-        t[0]=(TextView) findViewById(R.id.n_0);
-        t[1]=(TextView) findViewById(R.id.n_1);
-        t[2]=(TextView) findViewById(R.id.n_2);
-        t[3]=(TextView) findViewById(R.id.n_3);
-        t[4]=(TextView) findViewById(R.id.n_4);
-        t[5]=(TextView) findViewById(R.id.n_5);
-        t[6]=(TextView) findViewById(R.id.n_6);
-        t[7]=(TextView) findViewById(R.id.n_7);
-        t[8]=(TextView) findViewById(R.id.n_8);
-        t[9]=(TextView) findViewById(R.id.n_9);
-        t[10]=(TextView) findViewById(R.id.n_10);
-        t[11]=(TextView) findViewById(R.id.n_11);
-        t[12]=(TextView) findViewById(R.id.n_12);
-        t[13]=(TextView) findViewById(R.id.n_13);
-        t[14]=(TextView) findViewById(R.id.n_14);
-        t[15]=(TextView) findViewById(R.id.n_15);
-        t[16]=(TextView) findViewById(R.id.n_16);
-        for(int i=0;i<17;i++){
-            t[i].setOnClickListener(this);
-        }
-
+        ed = (EditText)findViewById(R.id.edit_text);
+        send = (Button)findViewById(R.id.n_12);
+        send.setOnClickListener(this);
+        Log.d("dd","789");
     }
 
-    @Override
-    public void onClick(View v) {
 
+    public void save(String str){
+        String data=str;
+        FileOutputStream out=null;
+        BufferedWriter writer=null;
+
+        try{
+            out =openFileOutput("wechat", Context.MODE_PRIVATE);
+            writer=new BufferedWriter(new OutputStreamWriter(out));
+            Log.d("dd","456");
+            writer.write(data);
+        }   catch(IOException e){
+            e.printStackTrace();
+        }   finally {
+                   if (writer !=null){
+                       try {
+                           writer.close();
+                       } catch (IOException e) {
+                           e.printStackTrace();
+                       }
+                   }
+        }
+    }
+    @Override
+    public void  onClick(View view){
+        if(view==send){
+            String str =ed.getText().toString();
+            this.save(str);
+            Log.d("dd","123");
+        }
+//        for(int i=0;i<17;i++){
+//            String str =t[i].getText().toString();
+//            this.save(str);
+//            String s=couting.getText().toString();
+//            ed.setText(s);
+//
+//        }
+//        if(t[i]==view){
+//            String str =t[i].getText().toString();
+//            counting.append(str);
+//        }
     }
 }
