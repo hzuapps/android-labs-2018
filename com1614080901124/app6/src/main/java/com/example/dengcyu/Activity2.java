8689d3a419a5f7c47d4296b967f1770441c58564
dencyu <dengcyu@foxmail.com>
2018-06-02 23:00:48 +0800

    实验四、五、六

---
+++ com1614080901124/app6/src/main/java/com/example/dengcyu/Activity2.java
@@ -0,0 +1,158 @@
+package com.example.dengcyu;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+
+public class Activity2 extends AppCompatActivity {
+
+    public static final String DIRECTORY = "leaderboard";
+    public static final String FILENAME = "file_leaderboard.txt";
+
+    public static final String TAG =Activity2.class.getSimpleName();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_Film);
+
+        Button textView2 = (Button) findViewById(R.id.button_open3);
+        textView2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Activity2.this, com1614080901124Activity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button save = (Button) findViewById(R.id.save);
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String ranking = ((TextView) findViewById(R.id.ranking1)).getText().toString();
+                String name = ((TextView) findViewById(R.id.ranking1_name)).getText().toString();
+                String point = ((TextView) findViewById(R.id.ranking1_point)).getText().toString();
+                saveText(ranking,name,);
+            }
+        });
+
+        Button save= (Button) findViewById(R.id.save1);
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String r ="甄嬛传.";
+                String n ="孙俪";
+                String p ="观看";
+                ((TextView) findViewById(R.id.ranking1)).setText(r);
+                ((TextView) findViewById(R.id.ranking1_name)).setText(n);
+                ((TextView) findViewById(R.id.ranking1_point)).setText(p);
+                Toast.makeText(Activity2.this,"已更新："+r+" "+n+" "+p,Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void saveText(String _ranking,String _name,String _point) {
+        String blank = " ";
+        File dir = this.getFilesDir();  // 获取内部存储目录
+        File file = new File(dir, FILENAME);
+        FileOutputStream fos = null;    // 字节流  | char | cn : gbk 2 bytes, utf8 3 bytes
+
+        try { // 使用API打开输出流
+            fos = openFileOutput(FILENAME, MODE_PRIVATE);
+            //FileOutputStream fos = new FileOutputStream(file);
+            fos.write(_ranking.getBytes()); // 写入剧名
+            fos.write(' ');
+            fos.write(_name.getBytes()); // 写入主演名字
+            fos.write(' ');
+            fos.write(_point.getBytes()); // 写入资源
+            fos.write(' ');
+            fos.close(); // 关闭流
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                fos.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        Toast.makeText(Activity2.this,"数据已保存:"+_ranking+" "+_name+" "+_point,Toast.LENGTH_SHORT).show();
+    }
+    {
+
+        private ImageView imageView;
+        private Bitmap HeadPortrait;
+
+        public Bitmap getImage(String image_url){
+        Bitmap bitmap = null;
+        try{
+            URL url=new URL(image_url);
+            URLConnection connection = url.openConnection();
+            connection.connect();
+            InputStream inputStream = connection.getInputStream();
+            bitmap= BitmapFactory.decodeStream(inputStream);
+        } catch (java.net.MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return  bitmap;
+    }
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity2);
+
+        imageView = (ImageView) findViewById(R.id.background);
+        Resources res = Activity2.this.getResources();
+        space = BitmapFactory.decodeResource(res, R.drawable.space);
+
+
+        Button use_sky = (Button) findViewById(R.id.use_HeadPortrait);
+        use_sky.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                imageView.setImageBitmap(HeadPortrait);
+                Toast.makeText(Activity2.this,"设置为头像",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        Button download_sky = (Button) findViewById(R.id.download_HeadPortrait);
+        download_sky.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        tupian = getImage("https://github.com/dencyu/android-labs-2018/blob/master/HeadPortrait.jpg");
+                        try {
+                            Thread.sleep(2500);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }).start();
+                Toast.makeText(Activity2.this,Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+    }
+
+
+}
+
+}
+
