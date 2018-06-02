f3a53c6fe20ed117d1f0025b8ca66396a7e00363
aLesion <717876241@qq.com>
2018-06-02 11:35:20 +0800

    #2 惠大考试复习平台

---
+++ Soft1614080902114/app/src/main/java/edu/hzuapps/android/soft1614080902114/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.soft1614080902114;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
