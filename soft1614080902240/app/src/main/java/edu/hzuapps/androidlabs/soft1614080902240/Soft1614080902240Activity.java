856164df7835f10b179331a7e210d2a3ec4d3379
ycl0916 <37758478+ycl0916@users.noreply.github.com>
2018-06-02 12:42:22 +0800

    Add files via upload

--- soft1614080902240/app/src/main/java/edu/hzuapps/androidlabs/soft1614080902240/Soft1614080902240Activity.java
+++ soft1614080902240/app/src/main/java/edu/hzuapps/androidlabs/soft1614080902240/Soft1614080902240Activity.java
@@ -13,6 +13,7 @@ public class Soft1614080902240Activity extends AppCompatActivity {
     Button Button3;
     Button Button4;
     Button Button5;
+    Button Button6;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,6 +23,8 @@ public class Soft1614080902240Activity extends AppCompatActivity {
         Button3 = (Button)findViewById(R.id.button3);
         Button4 = (Button)findViewById(R.id.button4);
         Button5 = (Button)findViewById(R.id.button5);
+
+        Button6 = (Button)findViewById(R.id.button6);
 
 
         Button2.setOnClickListener(new View.OnClickListener() {
@@ -63,6 +66,17 @@ public class Soft1614080902240Activity extends AppCompatActivity {
                 if (v.equals(Button5)) {
                     // 另一个Activity的完整名称 = edu.androidlabs.soft123456(包名).Soft123456Activity(类名)
                     Intent intent = new Intent(Soft1614080902240Activity.this,Soft1614080902240Activity5.class);
+                    startActivity(intent);
+                }
+            }
+        });
+
+        Button6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (v.equals(Button6)) {
+                    // 另一个Activity的完整名称 = edu.androidlabs.soft123456(包名).Soft123456Activity(类名)
+                    Intent intent = new Intent(Soft1614080902240Activity.this,Soft1614080902240Activity6.class);
                     startActivity(intent);
                 }
             }
