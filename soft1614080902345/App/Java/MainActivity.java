4f1005341265c679f625f7e7eb04bf3c87300852
chinesehope <741713720@qq.com>
2018-06-02 21:49:52 +0800

    #56,实验5和实验6

--- soft1614080902345/App/Java/MainActivity.java
+++ soft1614080902345/App/Java/MainActivity.java
@@ -7,7 +7,7 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    private Button runbutton,run2button;
+    private Button runbutton,run2button,jsonbutton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         runbutton=(Button) findViewById(R.id.button1);
         run2button=(Button) findViewById(R.id.button2);
+        jsonbutton=(Button) findViewById(R.id.button);
         runbutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -26,6 +27,13 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent =new Intent(MainActivity.this,Run2.class);
+                startActivity(intent);
+            }
+        });
+        jsonbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent =new Intent(MainActivity.this,Json.class);
                 startActivity(intent);
             }
         });
