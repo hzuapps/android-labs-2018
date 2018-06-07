51fb7657c314361ce49e088139a68255d565c9cf
YoMiao <1055076808@qq.com>
2018-06-07 22:42:31 +0800

    第六次实验

--- com1614080901130/app/src/main/java/edu/hzuapps/androidlabs/Com1614080901130/Anni.java
+++ com1614080901130/app/src/main/java/edu/hzuapps/androidlabs/Com1614080901130/Anni.java
@@ -6,8 +6,6 @@ import android.widget.Button;
 import android.content.Intent;
 import android.view.View;
 import android.widget.TextView;
-//import android.widget.TextView;
-
 
 public class Anni extends Activity {
 
@@ -20,13 +18,23 @@ public class Anni extends Activity {
         final Activity thisActivity = this;
 
 
-        Button btnOpen =  findViewById(R.id.home_button);
-        btnOpen.setOnClickListener(new View.OnClickListener() {
+        Button btnOpen1 =  findViewById(R.id.home_button);
+        btnOpen1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(thisActivity,Show.class);
                 thisActivity.startActivity(intent);
             }
         });
+
+        Button btnOpen2 =  findViewById(R.id.download_button);
+        btnOpen2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(thisActivity,Download.class);
+                thisActivity.startActivity(intent);
+            }
+        });
+
     }
 }
---
+++ com1614080901130/report/report6.md
@@ -0,0 +1,21 @@
+﻿# 第六次实验
+
+## 1. 实验要求
+1. 从网络下载一个文件（图片、MP3、MP4）；
+2. 保存到手机，在应用中使用文件；
+3. 将应用运行结果截图。
+
+## 2. 实验步骤
+1. 先修改AndroidManifest.xml中的文件，以获取上网的相关权限；
+2. 在Donwload.java文件中实现连接网络获取图片，显示图片，图片保存功能；
+3. 定义connectNet类用以下载图片，new 一个线程Thread进行下载；
+4. 使用加载保存按钮获取URL的图片并保存；
+5. 用git上传到github。
+
+## 3. 实验结果
+输入一个图片的url地址然后点击保存加载，成功加载到一个图片。  
+![](https://github.com/YoMiao/android-labs-2018/blob/master/com1614080901130/screen/report6_screen1.png)  
+![](https://github.com/YoMiao/android-labs-2018/blob/master/com1614080901130/screen/report6_screen2.png)  
+
+## 4. 实验心得
+不懂的东西可以百度一下，或者去参考别人的，进一步学习。
